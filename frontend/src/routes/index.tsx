010fa4b1755a0c97de95c8178c4c10b756415cb3
yeonmi <ar77gt@daum.net>
2024-03-11 15:30:39 +0900

    feat : folder structure
    Former-commit-id: cea7dbc3bcdcbd143a65de217c284f9f1b61f964 [formerly 45c45168c7d49bf9faaa4e6da5c248543cd50efe]
    Former-commit-id: 7647374aade5e6bb2952a4ae43fc4cf534bd60ed

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home, { rootLoader } from "./routes/root";
+import Team, { teamLoader } from "./routes/team";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "team",
+        element: <Team />,
+        loader: teamLoader,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
+
+// https://reactrouter.com/en/main/routers/create-browser-router
