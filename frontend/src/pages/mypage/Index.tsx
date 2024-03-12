b96bcfa0123295178630df881c2dd6ace6b1c147
yeonmi <ar77gt@daum.net>
2024-03-12 17:01:02 +0900

    design : mypage index
    Former-commit-id: 83055450200f9b9f14f33e3ec6677a79cf21a1ab [formerly 5cfa8d81b5ce7e28bbf2bb893a27d1830c73db01]
    Former-commit-id: 94eecbbda7ab93c581079806aa1bb4c16839af16

--- frontend/src/pages/mypage/Index.tsx
+++ frontend/src/pages/mypage/Index.tsx
@@ -1,14 +1,59 @@
-import Header from "../../components/common/Header";
-import Recent from "../../components/mypage/Recent";
-import LevelTest from "../../components/mypage/LevelTest";
-import BookMark from "../../components/mypage/BookMark";
+import myImage from '../../assets/badge/badge1.png';
+import graph from '../../assets/graph.png';
 
 const Index = () => {
-  return <div>
-<div><Header/></div>
-<div><Recent/></div>
-<div><LevelTest/></div>
-<div><BookMark/></div>
+  return <div className="bg-[#212529]">
+    <div className="max-w-[700px] m-auto">
+
+      <div className="text-white font-semibold">
+        경험치
+      </div>
+      <div className="flex space-x-8">
+        <div className="bg-gradient-to-r text-white from-[#4EDBDE] from-5% to-[#5572A1] to-100% font-semibold flex rounded-xl overflow-hidden mb-1 w-60 h-60 flex justify-center items-center">
+          <div className="flex justify-items-center items-center">
+            <img src={myImage} width="100" height="100"></img>
+            만화좋아
+          </div>
+        </div>
+        <div>
+          <img src={graph} width="400" height="400" alt="이미지 설명" />
+        </div>
+      </div>
+
+      <div className="bg-gradient-to-r text-white font-semibold text-base flex items-center rounded-2xl mb-3 space-x-2">
+      <div className="text-white font-semibold">
+        LV1
+      </div>
+        <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% font-semibold flex rounded-xl overflow-hidden w-80 h-5">
+        </div>
+      </div>
+
+      <div className="text-white font-semibold">
+      단어장
+      </div>
+      <div className="flex space-x-5">
+        <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-4 flex justify-center items-center">
+        <div>
+          <img src={myImage} width="100" height="100" alt="이미지 설명" />
+          <p className="text-center">LV1</p>
+        </div>
+       </div>
+       <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-4 flex justify-center items-center">
+        <div>
+          <img src={myImage} width="100" height="100" alt="이미지 설명" />
+          <p className="text-center">LV1</p>
+        </div>
+       </div>
+       <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-4 flex justify-center items-center">
+        <div>
+          <img src={myImage} width="100" height="100" alt="이미지 설명" />
+          <p className="text-center">LV1</p>
+        </div>
+       </div>
+        
+      </div>
+     
+    </div>
   </div>
 }
 
