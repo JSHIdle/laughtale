ae5661d71c8d5e4ebadc9a800ca794e00e5bd6f1
yeonmi <ar77gt@daum.net>
2024-03-12 17:34:09 +0900

    desing : main, mypage header

--- frontend/src/pages/mypage/Index.tsx
+++ frontend/src/pages/mypage/Index.tsx
@@ -1,10 +1,14 @@
 import myImage from '../../assets/badge/badge1.png';
 import graph from '../../assets/graph.png';
+import MypageHeader from '../../components/common/mypageHeader';
 
 const Index = () => {
-  return <div className="bg-[#212529]">
+  return <div className="bg-[#212529] min-h-screen">
     <div className="max-w-[700px] m-auto">
 
+    <div>
+      <MypageHeader/>
+    </div>
       <div className="text-white font-semibold">
         경험치
       </div>
@@ -21,36 +25,35 @@ const Index = () => {
       </div>
 
       <div className="bg-gradient-to-r text-white font-semibold text-base flex items-center rounded-2xl mb-3 space-x-2">
-      <div className="text-white font-semibold">
-        LV1
-      </div>
+        <div className="text-white font-semibold">
+          LV1
+        </div>
         <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% font-semibold flex rounded-xl overflow-hidden w-80 h-5">
         </div>
       </div>
 
       <div className="text-white font-semibold">
-      단어장
+        단어장
       </div>
-      <div className="flex space-x-5">
-        <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-4 flex justify-center items-center">
+      <div className="flex">
+        <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-5 flex justify-center items-center">
         <div>
           <img src={myImage} width="100" height="100" alt="이미지 설명" />
           <p className="text-center">LV1</p>
         </div>
        </div>
-       <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-4 flex justify-center items-center">
+       <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-5 flex justify-center items-center">
         <div>
           <img src={myImage} width="100" height="100" alt="이미지 설명" />
           <p className="text-center">LV1</p>
         </div>
        </div>
-       <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-4 flex justify-center items-center">
+       <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-5 flex justify-center items-center">
         <div>
           <img src={myImage} width="100" height="100" alt="이미지 설명" />
           <p className="text-center">LV1</p>
         </div>
        </div>
-        
       </div>
      
     </div>
