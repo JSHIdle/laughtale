c52f13dd8d55fd2c2bcda21722009501d22a9dc6
yeonmi <ar77gt@daum.net>
2024-03-13 12:47:00 +0900

    design : mypage
    Former-commit-id: 2eaa5ecad865e31f8023f4e481078428a03c4cc5 [formerly 8634b0c3bdc25334c77d67724fd626604774ca9a]
    Former-commit-id: a8983893c30cae33c94cae0ffe7be513756b5d1b

--- frontend/src/pages/mypage/Index.tsx
+++ frontend/src/pages/mypage/Index.tsx
@@ -1,61 +1,68 @@
 import myImage from '../../assets/badge/badge1.png';
-import graph from '../../assets/graph.png';
+import myImage2 from '../../assets/badge/badge2.png';
+import myImage3 from '../../assets/badge/badge3.png';
 import MypageHeader from '../../components/common/mypageHeader';
+import DoughnutChart from "../../components/mypage/DoughnutChart.tsx";
+import { Progress } from "@material-tailwind/react";
+
+export function Bar() {
+  return (
+      <div className="flex w-full flex-col gap-4">
+        <Progress value={75} size="lg" placeholder={undefined} />
+      </div>
+  );
+}
 
 const Index = () => {
+
   return <div className="bg-[#212529] min-h-screen">
     <div className="max-w-[700px] m-auto">
-
     <div>
       <MypageHeader/>
     </div>
-      <div className="text-white font-semibold">
+      <div className="mt-12 text-white font-semibold">
         경험치
       </div>
-      <div className="flex space-x-8">
-        <div className="bg-gradient-to-r text-white from-[#4EDBDE] from-5% to-[#5572A1] to-100% font-semibold flex rounded-xl overflow-hidden mb-1 w-60 h-60 flex justify-center items-center">
+      <div className="flex space-x-12">
+        <div className="bg-gradient-to-r text-white from-[#4EDBDE] from-5% to-[#8675DA] to-100% font-semibold flex rounded-xl overflow-hidden mr-5 w-60 h-60 flex justify-center items-center">
           <div className="flex justify-items-center items-center">
             <img src={myImage} width="100" height="100"></img>
             만화좋아
           </div>
         </div>
         <div>
-          <img src={graph} width="400" height="400" alt="이미지 설명" />
+          <DoughnutChart/>
         </div>
       </div>
 
-      <div className="bg-gradient-to-r text-white font-semibold text-base flex items-center rounded-2xl mb-3 space-x-2">
         <div className="text-white font-semibold">
-          LV1
+            <Bar/>
         </div>
-        <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% font-semibold flex rounded-xl overflow-hidden w-80 h-5">
-        </div>
-      </div>
 
-      <div className="text-white font-semibold">
+      <div className="mt-12 text-white font-semibold">
         단어장
       </div>
       <div className="flex">
-        <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-5 flex justify-center items-center">
+        <div className="bg-gradient-to-b from-[#5BC9E0] from-5% to-[#8E87EA] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-5 flex justify-center items-center">
         <div>
           <img src={myImage} width="100" height="100" alt="이미지 설명" />
           <p className="text-center">LV1</p>
         </div>
        </div>
-       <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-5 flex justify-center items-center">
+       <div className="bg-gradient-to-b from-[#5BC9E0] from-5% to-[#8E87EA] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-5 flex justify-center items-center">
         <div>
-          <img src={myImage} width="100" height="100" alt="이미지 설명" />
-          <p className="text-center">LV1</p>
+          <img src={myImage2} width="100" height="100" alt="이미지 설명" />
+          <p className="text-center">LV2</p>
         </div>
        </div>
-       <div className="bg-gradient-to-r from-[#4EDBDE] from-5% to-[#5572A1] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-5 flex justify-center items-center">
+       <div className="bg-gradient-to-b from-[#5BC9E0] from-5% to-[#8E87EA] to-100% text-white font-semibold rounded-xl overflow-hidden mb-1 w-60 h-60 mr-5 flex justify-center items-center">
         <div>
-          <img src={myImage} width="100" height="100" alt="이미지 설명" />
-          <p className="text-center">LV1</p>
+          <img src={myImage3} width="100" height="100" alt="이미지 설명" />
+          <p className="text-center">LV3</p>
         </div>
        </div>
       </div>
-     
+
     </div>
   </div>
 }
