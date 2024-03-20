954f53180f6bc77844648ab8769e388c65b306cb
jinooyang <jinooyang@gmail.com>
2024-03-20 12:10:46 +0900

    rename : myquiz package -> quiz

--- backend/src/main/java/com/jshi/laughtale/quiz/controller/QuizController.java
+++ backend/src/main/java/com/jshi/laughtale/quiz/controller/QuizController.java
@@ -1,11 +1,11 @@
-package com.jshi.laughtale.myquiz.controller;
+package com.jshi.laughtale.quiz.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jshi.laughtale.myquiz.service.QuizService;
+import com.jshi.laughtale.quiz.service.QuizService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
