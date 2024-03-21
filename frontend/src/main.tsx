70e9bfbebefc18650932d4c9729b460476160f5a
Jaechan Kim <penguin_03@naver.com>
2024-03-21 15:14:21 +0900

    feature: 리액트쿼리 데브툴

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,17 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import "./index.css"
 import './styles/SliderStyles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+
 );
