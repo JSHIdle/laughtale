40be77d9ce7bbfae7f676650e86e63673d2e5474
jinooyang <jinooyang@gmail.com>
2024-03-21 10:38:21 +0900

    feat : quizResult
    Former-commit-id: 21ae890e6216b3359932309896a6e866563a8fdb [formerly 97481a6c4d0a84944a29f1ae19c3d67a646154ae]
    Former-commit-id: 3ab81c5684c6e7ca50d1e9bdcc9a25500e406e71

--- backend/src/main/java/com/jshi/laughtale/quiz/controller/QuizController.java
+++ backend/src/main/java/com/jshi/laughtale/quiz/controller/QuizController.java
@@ -1,6 +1,8 @@
 package com.jshi.laughtale.quiz.controller;
 
+import com.jshi.laughtale.quiz.dto.QuizResult;
 import com.jshi.laughtale.quiz.dto.QuizWord;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import com.jshi.laughtale.security.details.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -18,23 +21,34 @@ import java.util.List;
 @RequestMapping("/api/quiz")
 @RequiredArgsConstructor
 public class QuizController {
-    private final QuizService quizService;
+	private final QuizService quizService;
 
-    @GetMapping("/{chapterId}")
-    public ResponseEntity<List<QuizWord>> makeNewQuiz(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable int chapterId) {
-        //TODO : 해당 사용자가 퀴즈를 이미 만들었다면 삭제하고 진행한다 -> ex) 새로고침 상황
-        //새로운 퀴즈를 생성한다
-        List<QuizWord> quiz = quizService.makeNewQuiz(customUserDetails.getId(), chapterId);
-        //생성된 퀴즐르 DB에 저장한다
-        quizService.saveNewQuiz(quiz, customUserDetails.getId());
-        return ResponseEntity.ok(quiz);
-    }
+	@GetMapping("/{chapterId}")
+	public ResponseEntity<List<QuizWord>> makeNewQuiz(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+		@PathVariable int chapterId) {
+		//회원 아이디 조회
+		Long memberId = customUserDetails.getId();
+		//해당 사용자가 퀴즈를 이미 만들었다면 삭제하고 진행한다 -> ex) 새로고침 상황
+		quizService.deleteMemberQuiz(memberId);
+		//새로운 퀴즈를 생성한다
+		List<QuizWord> quiz = quizService.makeNewQuiz(memberId, chapterId);
+		//생성된 퀴즐르 DB에 저장한다
+		quizService.saveNewQuiz(quiz, memberId);
 
+		return ResponseEntity.ok(quiz);
+	}
 
-    @PostMapping("/{chapterId}")
-    public ResponseEntity<Void> quizResult(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable int chapterId) {
-        //TODO : 퀴즈 결과를 받아서 가중치가 변경되도록 기록한다. 또한, 퀴즈를 삭제한다. 퀴즈가 만약 존재하지 않는다면 반영 X
-        return ResponseEntity.ok().build();
-    }
+	@PostMapping("/solve")
+	public ResponseEntity<List<Integer>> quizResult(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+		@RequestBody QuizResult.Request request) {
+		Long memberId = customUserDetails.getId();
+		// TODO : 퀴즈 결과가 왔는데 퀴즈가 없던경우(예외처리)
+		//퀴즈 결과를 받아온다
+		List<Integer> result = quizService.saveQuizResult(memberId, request.getResult());
+
+		//저장된 퀴즈를 삭제한다
+		quizService.deleteMemberQuiz(memberId);
+		return ResponseEntity.ok(result);
+	}
 
 }
--- backend/src/main/java/com/jshi/laughtale/wordhistory/service/WordHistoryService.java
+++ backend/src/main/java/com/jshi/laughtale/wordhistory/service/WordHistoryService.java
@@ -1,16 +1,23 @@
 package com.jshi.laughtale.wordhistory.service;
 
+import com.jshi.laughtale.member.service.MemberService;
+import com.jshi.laughtale.worddata.domain.WordData;
 import com.jshi.laughtale.wordhistory.domain.WordHistory;
 import com.jshi.laughtale.wordhistory.repository.WordHistoryRepository;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class WordHistoryService {
 	private final WordHistoryRepository wordHistoryRepository;
+	private final MemberService memberService;
 
 	public List<WordHistory> getMemberWordHistory(Long memberId) {
 		return wordHistoryRepository.findByMemberId(memberId);
@@ -18,5 +25,32 @@ public class WordHistoryService {
 
 	public List<WordHistory> findByWordIdIn(List<Long> list) {
 		return wordHistoryRepository.findByWordDataIn(list);
+	}
+
+	public void addWordHistory(Long memberId, WordData wordData, int point) {
+		Optional<WordHistory> wordHistory =
+			wordHistoryRepository.findByMemberIdAndWordDataId(memberId, wordData.getId());
+		//이미 학습한 적이 있다면
+		if (wordHistory.isPresent()) {
+			WordHistory history = wordHistory.get();
+			//학습 횟수를 맞았으면 증가시키고 틀렸으면 증가시키지 않는다
+			history.setStudyCnt(history.getStudyCnt() + point);
+			//틀렸다면 OFFSET을 10 증가시키고 맞았으면 0으로 초기화시킨다
+			history.setOffset((history.getOffset() + 10) * Math.abs(1 - point));
+			history.setStudyDate(LocalDateTime.now());
+			wordHistoryRepository.save(history);
+		}
+		//최초 학습한 단어라면
+		else {
+			wordHistoryRepository.save(
+				WordHistory.builder()
+					.studyCnt(point)
+					.offset(10 * (1 - point))
+					.studyDate(LocalDateTime.now())
+					.member(memberService.findById(memberId))
+					.wordData(wordData)
+					.build()
+			);
+		}
 	}
 }
