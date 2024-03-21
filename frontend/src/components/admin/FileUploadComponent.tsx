b096ae554fa6196e708e49380616cdc5a774cd98
dohyeong <ehgud619@naver.com>
2024-03-21 17:33:35 +0900

    feature:admin 관리자 만화등록 페이지 제작
    Former-commit-id: e01ed49c13d502c1b0854b8f9058c50a4e9d3ffc [formerly b08745d1b9347d6e864c9391dc68fc00951dfe37]
    Former-commit-id: 1a4195783e582a6ec320e0c69c82323495aa45f8

--- frontend/src/components/admin/FileUploadComponent.tsx
+++ frontend/src/components/admin/FileUploadComponent.tsx
@@ -1,25 +1,21 @@
 // FileUploadComponent.tsx
-import { useState } from 'react';
+import  { useState } from 'react';
 
-export const FileUploadComponent = ({ thumbnailInputRef, filesInputRef }) => {
+export const FileUploadComponent = () => {
     const [thumbnailPreview, setThumbnailPreview] = useState('');
     const [comicFiles, setComicFiles] = useState([]);
     const [previewImage, setPreviewImage] = useState('');
 
     const handleThumbnailChange = (event) => {
-        const input = event.target as HTMLInputElement;
-        if (input.files && input.files[0]) {
-            const file = input.files[0];
+        if (event.target.files && event.target.files[0]) {
+            const file = event.target.files[0];
             setThumbnailPreview(URL.createObjectURL(file));
         }
-        console.log(thumbnailInputRef.current)
-        console.log(input.files[0])
     };
 
     const handleAddComicFiles = (event) => {
-        const input = event.target as HTMLInputElement;
-        const newFiles = Array.from(input.files || []);
-        setComicFiles((prevFiles) => [...prevFiles, ...newFiles]);
+        const newFiles = Array.from(event.target.files);
+        setComicFiles(prevFiles => [...prevFiles, ...newFiles]);
     };
 
     const handleFileClick = (file) => {
@@ -27,40 +23,34 @@ export const FileUploadComponent = ({ thumbnailInputRef, filesInputRef }) => {
     };
 
     const handleDeleteFile = (fileToDelete) => {
-        setComicFiles(comicFiles.filter((file) => file !== fileToDelete));
+        setComicFiles(comicFiles.filter(file => file !== fileToDelete));
     };
-
 
     return (
         <div className="flex gap-4">
             {/* Thumbnail Upload */}
-            <div className="w-1/3 bg-gray-800 p-6 rounded-lg mt-4 flex flex-col">
+            <div className="w-1/3 bg-gray-800 p-6 rounded-lg  mt-4 flex flex-col ">
                 <div className="mb-2 font-bold text-gray-300">썸네일 등록</div>
-                <label
-                    className="w-full h-60 bg-gray-500 bg-no-repeat bg-center bg-cover rounded border-2 border-blue-400 flex items-center justify-center cursor-pointer">
+                <label className="w-full h-60 bg-gray-500 bg-no-repeat bg-center bg-cover rounded border-2 border-blue-400 flex items-center justify-center cursor-pointer">
                     {thumbnailPreview ? (
-                        <img src={thumbnailPreview} alt="Thumbnail preview" className="rounded cursor-pointer"/>
+                        <img src={thumbnailPreview} alt="Thumbnail preview" className="rounded" />
                     ) : (
-                        <div className="flex flex-col items-center justify-center cursor-pointer">
-                            <span className="text-gray-400">클릭하여 이미지를 </span>
-                            <span className="text-gray-400">선택하세요</span>
-                            <span className="text-gray-400">355 x 485</span>
-                        </div>
+                        <>
+                            <div className="flex flex-col items-center justify-center">
+                                <span className="text-gray-400">클릭하여 이미지를</span>
+                                <span className="text-gray-400">선택하세요</span>
+                                <span className="text-gray-400">355 x 485</span>
+                            </div>
+                            <input type="file" className="hidden" onChange={handleThumbnailChange} accept="image/*" />
+                        </>
                     )}
-                    <input
-                        type="file"
-                        ref={thumbnailInputRef}
-                        className="hidden"
-                        onChange={handleThumbnailChange}
-                        accept="image/*"
-                    />
                 </label>
             </div>
 
             {/* Comic Files Upload */}
             <div className="w-1/3 bg-gray-800 p-6 rounded-lg mt-4 flex flex-col">
                 <div className="mb-2 font-bold text-gray-300">만화 파일 등록</div>
-                <div className="overflow-auto h-56 border-2 border-gray-500 rounded-lg p-2">
+                <div className="overflow-auto h-56  border-2 border-gray-500 rounded-lg p-2">
                     {comicFiles.map((file, index) => (
                         <div key={index} className="flex justify-between items-center p-1 hover:bg-gray-700 rounded">
                             <span onClick={() => handleFileClick(file)} className="cursor-pointer text-white truncate">
@@ -70,18 +60,25 @@ export const FileUploadComponent = ({ thumbnailInputRef, filesInputRef }) => {
                         </div>
                     ))}
                 </div>
-                <input type="file" multiple ref={filesInputRef} onChange={handleAddComicFiles} accept="image/*"
-                       className="cursor-pointer mt-2"/>
+                <input
+                    type="file"
+                    multiple
+                    onChange={handleAddComicFiles}
+                    accept="image/*"
+                    className="cursor-pointer mt-2"
+                />
             </div>
             {/* Preview Area */}
             <div className="w-1/3 bg-gray-800 p-6 rounded-lg mt-4">
                 <div className="mb-2 font-bold text-gray-300">미리보기</div>
-                <div className="w-full h-64 border-2 border-gray-500 rounded-lg">
+                <div className="w-full h-64  border-2 border-gray-500 rounded-lg">
                     {previewImage && (
-                        <img src={previewImage} alt="Preview" className="w-full h-full object-cover"/>
+                        <img src={previewImage} alt="Preview" className="w-full h-full object-cover" />
                     )}
                 </div>
             </div>
+
         </div>
+
     );
 };
