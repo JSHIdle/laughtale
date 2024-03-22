b096af212714d7c1220070894a4858b97753cb67
jinooyang <jinooyang@gmail.com>
2024-03-22 11:51:20 +0900

    feat : recent manga api and level manga api, refactor : manda entity(added level and thumbnail)
    Former-commit-id: 0ee7b4ef2b808a2cb849f1bef1051a2530ba1a59 [formerly d8dce6fca62afd7e7449ad9ef18f80882a9b3de7]
    Former-commit-id: 814fcc95a162795be28a433b016b9224688cece7

--- backend/src/main/java/com/jshi/laughtale/manga/service/MangaService.java
+++ backend/src/main/java/com/jshi/laughtale/manga/service/MangaService.java
@@ -1,18 +1,23 @@
 package com.jshi.laughtale.manga.service;
 
 import com.jshi.laughtale.manga.domain.Manga;
+import com.jshi.laughtale.manga.dto.LevelManga;
 import com.jshi.laughtale.manga.dto.MangaAnalyze;
 import com.jshi.laughtale.manga.dto.MangaUpload;
+import com.jshi.laughtale.manga.dto.RecentManga;
 import com.jshi.laughtale.manga.mapper.MangaMapper;
 import com.jshi.laughtale.manga.repository.MangaRepository;
 import com.jshi.laughtale.utils.DataRequest;
 import com.jshi.laughtale.utils.FileUtils;
 import com.jshi.laughtale.utils.MangaParser;
+
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -30,28 +35,37 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class MangaService {
 
-    private final MangaRepository mangaRepository;
-    private final MangaParser mangaParser;
+	private final MangaRepository mangaRepository;
+	private final MangaParser mangaParser;
 
-    public void upload(MultipartFile thumbnail, MangaUpload.Request manga, List<MultipartFile> files) throws IOException {
-        String thumbnailPath = FileUtils.save(thumbnail);
+	public void upload(MultipartFile thumbnail, MangaUpload.Request manga, List<MultipartFile> files) throws
+		IOException {
+		String thumbnailPath = FileUtils.save(thumbnail);
 
-        List<String> names = new ArrayList<>();
-        for (MultipartFile file : files) {
-            String filename = FileUtils.save(file);
-            names.add(filename);
-        }
+		List<String> names = new ArrayList<>();
+		for (MultipartFile file : files) {
+			String filename = FileUtils.save(file);
+			names.add(filename);
+		}
 
-        int last = mangaRepository.findLastChapterByManga(manga.getTitle()).orElse(0) + 1;
+		int last = mangaRepository.findLastChapterByManga(manga.getTitle()).orElse(0) + 1;
 
-        MangaAnalyze.Request analyzeRequest = MangaMapper.toAnalyze(thumbnailPath, manga, last, names);
-        Map result = DataRequest.analyze(analyzeRequest);
+		MangaAnalyze.Request analyzeRequest = MangaMapper.toAnalyze(thumbnailPath, manga, last, names);
+		Map result = DataRequest.analyze(analyzeRequest);
 
-        Manga m = mangaRepository.findByTitle(manga.getTitle())
-                .orElse(MangaMapper.analyzeToEntity(analyzeRequest));
-        mangaRepository.save(m);
+		Manga m = mangaRepository.findByTitle(manga.getTitle())
+			.orElse(MangaMapper.analyzeToEntity(analyzeRequest));
+		mangaRepository.save(m);
 
-        mangaParser.parser(m, result, last);
-        m.update();
-    }
+		mangaParser.parser(m, result, last);
+		m.update();
+	}
+
+	public List<RecentManga.Response> getRecentManga(Long memberId) {
+		return mangaRepository.findRecentManga(memberId);
+	}
+
+	public List<LevelManga.Response> getLevelManga(int level, int start, int end) {
+		return mangaRepository.findLevelManga(level, start, end).orElseThrow();
+	}
 }
