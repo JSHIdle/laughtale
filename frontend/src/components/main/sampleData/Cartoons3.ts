3ea817fd00d37b4b8f807aec88d71fe94644c2bc
Jaechan Kim <penguin_03@naver.com>
2024-03-22 15:06:28 +0900

    fix: path수정3
    Former-commit-id: 4c76798a5c28cd703f837396a4de9a640d7667cf [formerly b3770fecefad4a27932fe7f4c3c6b76505861902]
    Former-commit-id: b7b684e40fef1dcc44eb21264abf4d05a2db43cf

--- frontend/src/components/main/sampleData/Cartoons3.ts
+++ frontend/src/components/main/sampleData/Cartoons3.ts
@@ -1,27 +1,27 @@
 const cartoons2 = [
     {
-        imageUrl: '/assets/samples/e3-1.jpg',
+        imageUrl: '/src/assets/samples/e3-1.jpg',
         title: 'First Cartoon Title',
         authors: ['First Author'],
         rating: 9.4,
         link: 'https://comic.naver.com/webtoon',
     },
     {
-        imageUrl: '/assets/samples/e3-2.jpg',
+        imageUrl: '/src/assets/samples/e3-2.jpg',
         title: 'Second Cartoon',
         authors: ['Second Author'],
         rating: 8.7,
         link: 'https://comic.naver.com/webtoon',
     },
     {
-        imageUrl: '/assets/samples/e3-3.jpg',
+        imageUrl: '/src/assets/samples/e3-3.jpg',
         title: 'Second Cartoon',
         authors: ['Second Author'],
         rating: 8.7,
         link: 'https://comic.naver.com/webtoon',
     },
     {
-        imageUrl: '/assets/samples/e3-4.jpg',
+        imageUrl: '/src/assets/samples/e3-4.jpg',
         title: 'Second Cartoon',
         authors: ['Second Author'],
         rating: 8.7,
