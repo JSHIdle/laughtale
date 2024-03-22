f191e39a7a4bfb3173027c78690ab472736eb180
yeonmi <ar77gt@daum.net>
2024-03-22 17:31:46 +0900

    design: word

--- frontend/src/pages/mypage/word/Index.tsx
+++ frontend/src/pages/mypage/word/Index.tsx
@@ -5,7 +5,7 @@ import Header from "../../../components/common/Header.tsx";
 const Index = () => {
   const dummyData = Array.from({ length: 24 }, (_, index) => ({
     id: index,
-    content: `あ-う [会う]
+    content: `あ-う [会う] 
 동사 1.만나다
 2.대면하다;면회하다
 3.우연히 만나다;조우하다`,
@@ -23,24 +23,23 @@ const Index = () => {
 
   // CustomSlider 컴포넌트에 slideData를 slides props로 전달
   return(
-  <div className="bg-[#1D1D21] min-h-screen">
-    <div className="max-w-[1100px] m-auto">
+      <div className="bg-[#1D1D21] min-h-screen">
+        <div><Header/></div>
+        <div className="flex justify-center items-center">
+          <div className="max-w-[1100px] w-full">
+            <div className="flex justify-center items-center">
+              <div className="mt-3 w-[900px] text-white font-bold flex justify-start">
+                {/*<img src={myImage} width="50" height="50"/>*/}
+                LV1
+              </div>
+            </div>
 
-      <div><Header/></div>
-
-      <div className="flex justify-center items-center">
-        <div className="mt-3 w-[46px] text-white font-bold flex justify-center">
-          {/*<img src={myImage} width="50" height="50"/>*/}
-          LV1
+            <div>
+              <CustomSlider slides={slideDatas}/>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div>
-        <CustomSlider slides={slideDatas}/>
-      </div>
-
-    </div>
-  </div>
   );
 };
 
