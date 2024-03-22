2aa1fb43df0a70e9243ebab6df84f447049962c8
yeonmi <ar77gt@daum.net>
2024-03-22 17:27:23 +0900

    feat : view cartoon

--- backend/src/main/java/com/jshi/laughtale/cut/repository/CutRepository.java
+++ backend/src/main/java/com/jshi/laughtale/cut/repository/CutRepository.java
@@ -1,7 +1,19 @@
 package com.jshi.laughtale.cut.repository;
 
+<<<<<<< HEAD
 import com.jshi.laughtale.cut.domain.Cut;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CutRepository extends JpaRepository<Cut, Long> {
 }
+=======
+import com.jshi.laughtale.chapter.domain.Chapter;
+import com.jshi.laughtale.cut.domain.Cut;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CutRepository extends JpaRepository<Cut, Long> {
+    List<Cut> findByChapter(Chapter chapter);
+}
+>>>>>>> b4f6e56 (feat : view cartoon)
