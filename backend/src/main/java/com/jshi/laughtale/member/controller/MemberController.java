9ffae7ecfa59a7b5dc6a368567facd3b7bec1009
chlwodyd9829 <chlwodyd9829@naver.com>
2024-03-25 10:53:56 +0900

    fix: 소설로그인 API 연결 및 Member 필요 없는 메소드 정리
    Former-commit-id: 0f722809ae11ce83d8bc2915492d2e9659ea37d2 [formerly c2987f8117d70dcb357a84c8cb512cc39f7e4363]
    Former-commit-id: 8870ffd5f29c69bb8fe96f4717d1e53c6356ea27

--- backend/src/main/java/com/jshi/laughtale/member/controller/MemberController.java
+++ backend/src/main/java/com/jshi/laughtale/member/controller/MemberController.java
@@ -1,7 +1,5 @@
 package com.jshi.laughtale.member.controller;
 
-import com.jshi.laughtale.member.dto.MemberLogin;
-import com.jshi.laughtale.member.dto.MemberSignup;
 import com.jshi.laughtale.member.dto.MemberUpdate;
 import com.jshi.laughtale.member.service.MemberService;
 import com.jshi.laughtale.security.details.CustomUserDetails;
@@ -9,12 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -22,18 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
-
-    @PostMapping("/login")
-    public ResponseEntity<MemberLogin.Response> login(@RequestBody MemberLogin.Request login) {
-        return ResponseEntity.ok(memberService.login(login));
-    }
-
-    //todo : 소셜 로그인 사용자 후 처리 해야함
-    @PostMapping("/signup")
-    public ResponseEntity<Void> signup(@RequestBody MemberSignup.Request signup) {
-        memberService.signup(signup);
-        return ResponseEntity.ok().build();
-    }
 
     @PatchMapping("/modify")
     public ResponseEntity<Void> update(
