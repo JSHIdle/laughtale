2fba73b6aa5f4ff0c2299e0de2642c8e9073ab3d
yeonmi <ar77gt@daum.net>
2024-03-25 10:30:19 +0900

    feat : rebase
    Former-commit-id: 4ac9b258a54768639ad772e0769f0bb237957520 [formerly 5c3313763345ed5cf50aa69858e4d881b2ba214f]
    Former-commit-id: b3aadfa7c4aa66a8e42577ad7ce58230391d0251

--- backend/src/main/java/com/jshi/laughtale/cut/repository/CutRepository.java
+++ backend/src/main/java/com/jshi/laughtale/cut/repository/CutRepository.java
@@ -1,19 +1,20 @@
 package com.jshi.laughtale.cut.repository;
 
-<<<<<<< HEAD
+
+import com.jshi.laughtale.chapter.domain.Chapter;
 import com.jshi.laughtale.cut.domain.Cut;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CutRepository extends JpaRepository<Cut, Long> {
-}
-=======
 import com.jshi.laughtale.chapter.domain.Chapter;
 import com.jshi.laughtale.cut.domain.Cut;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.List;
+
+
 
 public interface CutRepository extends JpaRepository<Cut, Long> {
     List<Cut> findByChapter(Chapter chapter);
 }
->>>>>>> b4f6e56 (feat : view cartoon)
+
