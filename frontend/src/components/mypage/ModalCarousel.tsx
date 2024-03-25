8caf093f3faffb97106a23a1f93b4b2d9a463c01
yeonmi <ar77gt@daum.net>
2024-03-25 15:55:19 +0900

    merge
    Former-commit-id: 08e2b5f67ea84ddbe18a8f9403a8eabf4abdb30d [formerly 196d509c37a24d0bcadfc429ebf4c68f0d951964]
    Former-commit-id: 578a05af86e07e2e0132f51c44f8e952cba18ee5

---
+++ frontend/src/components/mypage/ModalCarousel.tsx
@@ -0,0 +1,71 @@
+import Slider from 'react-slick';
+import image1 from '../../assets/samples/e1-1.jpg';
+import image2 from '../../assets/samples/e1-3.jpg';
+import image3 from '../../assets/samples/e1-2.jpg';
+
+const ModalCarousel = () => {
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        className: "slider variable-width",
+        variableWidth: true,
+        adaptiveHeight: true,
+    };
+
+    const comics = [
+        {
+            id: 1,
+            title: "만화1",
+            episode: "3화",
+            imageUrl: image1,
+        },
+        {
+            id: 2,
+            title: "만화2",
+            episode: "3화",
+            imageUrl: image2,
+        },
+        {
+            id: 3,
+            title: "만화3",
+            episode: "3화",
+            imageUrl: image3,
+        },
+    ];
+
+    return (<div>
+        <div className="w-[300px]">
+            <Slider {...settings}>
+                {comics.map(book => (
+                    <div className="flex justify-center items-center">
+                    <div key={book.id} className="p-3">
+                        <img src={book.imageUrl} style={{ width: "100%", height: "auto" }} alt={book.title}/>
+                    </div>
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    </div>
+    );
+}
+
+export default ModalCarousel;
+
+
+
+//     return (
+//         <div className="flex justify-center items-center h-full">
+//             <Slider {...settings}>
+//                 {comics.map(book => (
+//                     <div key={book.id}>
+//                         <img src={book.imageUrl} alt={book.title} style={{ width: "100%", height: "auto" }}/>
+//                     </div>
+//                 ))}
+//             </Slider>
+//         </div>
+//     );
+// }
