876928e8ed74a953b43b5dca711f6ab00c51ba47
chlwodyd9829 <chlwodyd9829@naver.com>
2024-03-26 10:12:21 +0900

    feat: 멤버 로그인 기능 추가

--- backend/src/main/java/com/jshi/laughtale/security/jwt/JwtProcessor.java
+++ backend/src/main/java/com/jshi/laughtale/security/jwt/JwtProcessor.java
@@ -47,7 +47,7 @@ public class JwtProcessor implements BeanPostProcessor {
         Claims claims = Jwts.claims()
                 .setSubject(email)
                 .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + Duration.ofDays(1).toNanos()));
+                .setExpiration(new Date(now.getTime() + Duration.ofDays(60).toNanos()));
         return Jwts.builder()
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
                 .setClaims(claims)
