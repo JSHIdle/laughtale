494bc729bf181a6d4e0639bdea78482ff20bc877
jaechan <penguin_03@naver.com>
2024-03-27 20:48:42 +0900

    feat: auth끝 / redirect만 확인하면 끝

--- frontend/src/pages/login/UserInfoFetcher.tsx
+++ frontend/src/pages/login/UserInfoFetcher.tsx
@@ -1,19 +1,23 @@
 import {useQuery, useSuspenseQuery} from "@tanstack/react-query";
 import {getMyInfo} from "../../apis/auth.ts";
-import {Navigate, Outlet} from "react-router-dom";
-import Spinner from "../../components/common/Spinner.tsx";
+import {Navigate} from "react-router-dom";
 import {useAuth} from "../../stores/useAuth.ts";
-import useAuthLocalStroage from "../../stores/useAuthLocalStroage.ts";
-import useLocalStorage from "../../stores/useLocalStorage.ts";
+import {Role} from "../../constants/Role.ts";
 type Props = {
-  accessToken?: string;
+  accessToken: string;
+}
+
+const tmpToken = "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJwaWUiLCJleHAiOjEwNzExNTM2NTIyLCJzdWIiOiJ3b2Nrc18yQGhhbm1haWwubmV0Iiwicm9sZXMiOiJST0xFX05PVF9DRVJUSUZJRUQifQ.QS3f6js8xqqPz98Hjc4SJId8mgAUIq7AuogbGzbVcGY&refreshToken=eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJwaWUiLCJleHAiOjM2MDE3MTE1MzY1MjIsInN1YiI6IndvY2tzXzJAaGFubWFpbC5uZXQiLCJyb2xlcyI6IlJPTEVfTk9UX0NFUlRJRklFRCJ9._PQjdz9pt3qOz0Q0r2orUXC4iukZZUcjLqfY0dSCj94";
+
+const tmpUser = {
+  iss: "pie",
+  exp: 10711536522,
+  sub: "wocks_2@hanmail.net",
+  roles: "ROLE_NOT_CERTIFIED"
 }
 const UserInfoFetcher = (props : Props) => {
-  const {user} = useAuth();
+  const {user, setUser, setToken} = useAuth();
   const {accessToken} = props;
-  const local = useLocalStorage("accessToken");
-  const authLocalStroage = useAuthLocalStroage();
-  authLocalStroage.set( {accessToken})
   const {data} = useSuspenseQuery({
     queryKey: ["auth"],
     queryFn:() => getMyInfo(),
@@ -21,8 +25,8 @@ const UserInfoFetcher = (props : Props) => {
   });
 
   if(data){
-    console.log(data);
-    localStorage.setItem("accessToken",JSON.stringify({accessToken}));
+    setToken({accessToken});
+    setUser({id:1, nickname:'test', profile:'test', role: Role.USER});
     return <Navigate to="/home"/>
   }else {
     return <Navigate to="/error"/>
--- frontend/src/stores/useAuth.ts
+++ frontend/src/stores/useAuth.ts
@@ -1,18 +1,26 @@
 import { create } from "zustand"
 import {Token, User} from "../../types";
+import useAuthLocalStorage from "./useAuthLocalStorage.ts";
 type AuthState  ={
     user : User
     token: Token
 }
 
-type AuthAction  ={
+type AuthAction  = {
     setUser: (user: User) => void;
+    setToken: (token: Token) => void;
+
 }
 
 type AuthStore = AuthState & AuthAction;
 
-export const useAuth = create<AuthStore>((set) => ({
-    user: null,
-    token: null,
-    setUser: (user) => set(() => ({user})),
-}))
+export const useAuth = create<AuthStore>((set) => {
+    const authLocalStorage = useAuthLocalStorage();
+    console.log(authLocalStorage.get())
+    return {
+        user: null,
+        token: authLocalStorage.get(),
+        setUser: (user) => set(() => ({user})),
+        setToken: (token: Token) => authLocalStorage.set({...token}),
+    }
+})
