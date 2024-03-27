f70ffbb6a08d8c03668f5a970bb6bb319eb2c289
jinooyang <jinooyang@gmail.com>
2024-03-27 09:56:19 +0900

    fix : api/history/{title}/{chapterNo} - refactor : viewHistoryTable @Prepersist localdatetime
    Former-commit-id: 0729133f1bd0b02a3194d8c79ea16b62fc43cd09 [formerly 40ce5797c0f2d4ce2fba563e491a07e68c4ff878]
    Former-commit-id: 41ae76a8bf37749080ffe8a29fef107f98390949

--- backend/src/main/java/com/jshi/laughtale/viewhistory/service/ViewHistoryService.java
+++ backend/src/main/java/com/jshi/laughtale/viewhistory/service/ViewHistoryService.java
@@ -1,6 +1,8 @@
 package com.jshi.laughtale.viewhistory.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import com.jshi.laughtale.chapter.domain.Chapter;
 import com.jshi.laughtale.chapter.service.ChapterService;
@@ -23,10 +25,23 @@ public class ViewHistoryService {
     public void createHistory(String title, Integer chapterNo, Long userId) {
         Member member = memberService.findById(userId);
         Chapter chapter = chapterService.loadByTitleAndChapterNo(title, chapterNo);
-        List<ViewHistory> existingHistories = viewHistoryRepository.findAllByMemberAndChapter(member, chapter).orElse(null);
-        if (existingHistories == null || existingHistories.isEmpty()) {
+        Optional<List<ViewHistory>> existingHistoriesOptional = viewHistoryRepository.findAllByMemberAndChapter(member, chapter);
+        // if (existingHistories == null || existingHistories.isEmpty()) {
+        //     viewHistoryRepository.save(ViewHistoryMapper.toEntity(member, chapter));
+        // }
+        if (existingHistoriesOptional.isPresent() && !existingHistoriesOptional.get().isEmpty()) {
+            List<ViewHistory> existingHistories = existingHistoriesOptional.get();
+
+            // Update the date of existing history entries to the current date
+            for (ViewHistory history : existingHistories) {
+                history.setViewDate(LocalDateTime.now());
+            }
+
+            // Save the updated histories back to the repository
+            viewHistoryRepository.saveAll(existingHistories);
+        } else {
+            // If no existing histories, create a new history entry
             viewHistoryRepository.save(ViewHistoryMapper.toEntity(member, chapter));
         }
-
     }
 }
