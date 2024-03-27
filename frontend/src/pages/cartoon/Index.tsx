01c42bd76588140ed8c7f2de08b0f327f7ec5bdd
Jaechan Kim <penguin_03@naver.com>
2024-03-27 09:31:39 +0900

    feat: 만화 정보 가져오기 api 추가 및 타입 수정
    Former-commit-id: 359d7f4df9790146bc92f8601c4794af7c3260f1 [formerly 2fef271ed2a8745f9f5ac9f394ea611c1236f7b9]
    Former-commit-id: 0e6d02f8d16a460fa9a15695052fc0acf9e8f241

--- frontend/src/pages/cartoon/Index.tsx
+++ frontend/src/pages/cartoon/Index.tsx
@@ -1,70 +1,61 @@
 import Header from "../../../src/components/common/Header.tsx";
 import {useInfiniteQuery, useQuery} from "@tanstack/react-query";
-import {useParams, useSearchParams} from "react-router-dom";
-import {getMangaInfo, getChapterList, ChapterListRequest} from "../../apis/cartoon.ts";
-import CartoonHeader from "../../components/cartoon/CartoonHeader.tsx";
-import ChapterList from "../../components/cartoon/ChapterList.tsx";
-import {ChapterListResponse} from "../../types/types";
-import {useEffect} from "react";
-import {useInView} from "react-intersection-observer";
+import {useSearchParams} from "react-router-dom";
+import {getMangaInfo, getChapterList} from "../../apis/cartoon.ts";
 
 const Index = () => {
-  const params = useParams()
-  const mangaId = + params.title;
+  const [searchParams, ] = useSearchParams();
+  const mangaId = + searchParams.get("manga");
   const {data: mangaInfo, isLoading: mangaInfoLoading} = useQuery({
     queryKey: ["manga", mangaId],
-    queryFn: () => getMangaInfo(+ mangaId),
+    queryFn: () => getMangaInfo(mangaId),
   });
+  //
+  // const {
+  //   data,
+  //   error,
+  //   fetchNextPage,
+  //   hasNextPage,
+  //   isFetching,
+  //   isFetchingNextPage,
+  //   status,
+  // } = useInfiniteQuery({
+  //   queryKey: ['chapterList', mangaId],
+  //   queryFn: ({pageParam}) => getChapterList({
+  //     page: pageParam,
+  //     mangaId
+  //   }),
+  //   initialPageParam: 0,
+  //   getNextPageParam: (lastPage, allPages, lastPageParam) => {
+  //     if (lastPageParam === 0) {
+  //       return undefined
+  //     }
+  //     return lastPageParam + 1
+  //   },
+  // })
 
-  const {
-    data,
-    error,
-    fetchNextPage,
-    hasNextPage,
-    isFetching,
-    isFetchingNextPage,
-    status,
-  } = useInfiniteQuery<ChapterListResponse>({
-    queryKey: ['chapterList', mangaId],
-    queryFn: ({pageParam}) => {
-      // console.log("page param " + pageParam);
-      return getChapterList({
-        page: + pageParam,
-        mangaId
-      })
-    },
-    initialPageParam: 0,
-    getNextPageParam: (lastPage, allPages, lastPageParam) => {
-      // console.log("lastPageParam ", lastPageParam)
-      // console.log("lastpage" , lastPage);
-      // console.log("allPage ", allPages);
-      if(typeof lastPageParam !== 'number') {
-        return 0;
-      }
-      return lastPageParam + 1
-    },
-  });
-  const {ref, inView} = useInView({
-    threshold: 0,
-    triggerOnce: false
-  })
-  useEffect(() => {
-    console.log("Data", data)
-  }, [data]);
-  useEffect(() => {
-    console.log("TEST",inView)
-      if(inView){
-        fetchNextPage();
-      }
-
-  }, [inView]);
 
   return <div className="bg-[#1D1D21] min-h-screen">
     <div>
       <Header/>
     </div>
       <div className="max-w-[700px] m-auto">
-        { mangaInfoLoading ? <div>laoding...</div> : <CartoonHeader {...mangaInfo}/>}
+        <div
+            className="mt-6 flex rounded-3xl overflow-hidden bg-gradient-to-r from-[#64BEE2] from-5%  to-[#8395E8] to-100%">
+          <div className="w-5/12">
+            <img
+                src="https://i.namu.wiki/i/5vdfnFXt4GI-HGqkyz5llKd7QzfpCBxJVyw4MZ4FZuQCG54-Rom6qpaNJ0gUjYEmGaOm31_X8Rei-0uL7zU3DfwvFwnUrEjWeDSiJbZknES6hd9skeI7r59G2-EDI0ZuuXARfjjXkmP5MkiS0IKJcg.webp"/>
+          </div>
+          <div className="p-10">
+            <div className="font-bold text-3xl text-white	mb-10">{mangaInfo.title}</div>
+            <div className="font-bold text-xl text-white">작가 : {mangaInfo.author}</div>
+            <div className="font-bold text-xl text-white">장르 : {mangaInfo.genre}</div>
+            <div className="mb-10"></div>
+            <div className="font-bold text-xl text-white">줄거리</div>
+            {/*<div className="mb-10"></div>*/}
+            <div className="font-bold text-xl text-white">{mangaInfo.summary}</div>
+          </div>
+        </div>
         <div
           className="text-xl font-semibold text-center p-2 bg-gradient-to-r from-[#64BEE2] from-5%   to-[#8395E8] to-100% mt-10 mb-5 rounded-full">
           첫화보기 1화
@@ -75,19 +66,49 @@ const Index = () => {
           </div>
         </div>
         <div className="mb-3">
-          { !data ? <div>...loading..</div> :
-            <>
-              {
-                data.pages.map((pages) => <>
-                  <ChapterList content={pages.content} title={mangaInfo.title}/>
-                  <div ref={ref}></div>
-                </>)}
+          <div
+            className="bg-gradient-to-r from-[#747982] to-[#879099] text-white font-semibold text-xl flex p-5 items-center rounded-2xl mb-5">
+            <div className="w-[64px] rounded-xl overflow-hidden mr-5">
+              <img
+                  src="https://image-comic.pstatic.net/webtoon/648419/426/thumbnail_202x120_6ea6018a-5843-4020-ac46-7c2bb34b583c.jpg"/>
+            </div>
+            <div>1. 미지의 숲</div>
+          </div>
 
-            </>
-          }
-          {
-            isFetchingNextPage ?? <div className="text-white"> fetching...</div>
-          }
+          <div
+              className="bg-gradient-to-r from-[#64BEE2] from-5%   to-[#8395E8] to-100% font-bold text-xl flex p-5 items-center rounded-2xl mb-5">
+            <div className="w-[64px] rounded-xl overflow-hidden mr-5">
+              <img
+                  src="https://image-comic.pstatic.net/webtoon/648419/426/thumbnail_202x120_6ea6018a-5843-4020-ac46-7c2bb34b583c.jpg"/>
+            </div>
+            <div>2. 미지의 숲</div>
+          </div>
+
+          <div
+              className="bg-gradient-to-r from-[#747982] to-[#879099] text-white font-semibold text-xl flex p-5 items-center rounded-2xl mb-5">
+            <div className="w-[64px] rounded-xl overflow-hidden mr-5">
+              <img
+                  src="https://image-comic.pstatic.net/webtoon/648419/426/thumbnail_202x120_6ea6018a-5843-4020-ac46-7c2bb34b583c.jpg"/>
+            </div>
+            <div>1. 미지의 숲</div>
+          </div>
+          <div
+              className="bg-gradient-to-r from-[#747982] to-[#879099] text-white font-semibold text-xl flex p-5 items-center rounded-2xl mb-5">
+            <div className="w-[64px] rounded-xl overflow-hidden mr-5">
+              <img
+                  src="https://image-comic.pstatic.net/webtoon/648419/426/thumbnail_202x120_6ea6018a-5843-4020-ac46-7c2bb34b583c.jpg"/>
+            </div>
+            <div>1. 미지의 숲</div>
+          </div>
+
+          <div
+              className="bg-gradient-to-r from-[#747982] to-[#879099] text-white font-semibold text-xl flex p-5 items-center rounded-2xl mb-5">
+            <div className="w-[64px] rounded-xl overflow-hidden mr-5">
+              <img
+                  src="https://image-comic.pstatic.net/webtoon/648419/426/thumbnail_202x120_6ea6018a-5843-4020-ac46-7c2bb34b583c.jpg"/>
+            </div>
+            <div>1. 미지의 숲</div>
+          </div>
         </div>
       </div>
     </div>
