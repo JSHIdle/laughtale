edbb81723918d379a535bb24f8331d8d2e66938b
Jaechan Kim <penguin_03@naver.com>
2024-03-27 10:13:00 +0900

    feat: auth button2

--- frontend/src/pages/login/UserInfoFetcher.tsx
+++ frontend/src/pages/login/UserInfoFetcher.tsx
@@ -2,16 +2,28 @@ import {useQuery, useSuspenseQuery} from "@tanstack/react-query";
 import {getMyInfo} from "../../apis/auth.ts";
 import {Navigate, Outlet} from "react-router-dom";
 import Spinner from "../../components/common/Spinner.tsx";
+import {useAuth} from "../../stores/useAuth.ts";
+import useAuthLocalStroage from "../../stores/useAuthLocalStroage.ts";
+import useLocalStorage from "../../stores/useLocalStorage.ts";
 type Props = {
   accessToken?: string;
 }
 const UserInfoFetcher = (props : Props) => {
+  const [user,] = useAuth();
+  const {accessToken} = props;
+  const local = useLocalStorage("accessToken");
+  const authLocalStroage = useAuthLocalStroage();
+  authLocalStroage.set( {accessToken})
   const {data} = useSuspenseQuery({
     queryKey: ["auth"],
-    queryFn:() => getMyInfo({accessToken: props}),
+    queryFn:() => getMyInfo(),
     retry: 0,
   });
 
-  return <div>tset</div>;
+  if(data){
+    console.log(data);
+    localStorage.setItem("accessToken",JSON.stringify({accessToken}));
+    return <Navigate to="/home"/>
+  }
 }
 export default UserInfoFetcher;
