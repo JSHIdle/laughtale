d41fa348e4bf1b391e214a8e546c8b9e869fdd81
jinooyang <jinooyang@gmail.com>
2024-03-28 17:33:45 +0900

    fix : quiz

--- backend/src/main/java/com/jshi/laughtale/ebbinghaus/EbbinghausUtils.java
+++ backend/src/main/java/com/jshi/laughtale/ebbinghaus/EbbinghausUtils.java
@@ -5,10 +5,13 @@ import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 /*
 에빙하우스 망각 곡선 계산
 */
 @Service
+@Slf4j
 public class EbbinghausUtils {
 
 	//현재 단어를 기억할 확률을 반환한다
@@ -19,6 +22,7 @@ public class EbbinghausUtils {
 	private Double calculateMinutesPassed(LocalDateTime lastStudyTime) {
 		LocalDateTime currentTime = LocalDateTime.now();
 		Duration duration = Duration.between(lastStudyTime, currentTime);
+		log.info("minutes passed : " + (double)duration.toMinutes());
 		return (double)duration.toMinutes();
 	}
 
@@ -27,7 +31,7 @@ public class EbbinghausUtils {
 		double c = 1.25;
 		double a = 2.0;
 		return (100 * k * Math.pow(a, studyCnt - 1)) /
-			(Math.pow(Math.log(minutesPassed), c) + k * Math.pow(a, studyCnt - 1));
+			(Math.pow(Math.log10(minutesPassed), c) + k * Math.pow(a, studyCnt - 1));
 	}
 
 }
