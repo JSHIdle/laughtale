06e00c0b5d929a4fe1a107160b28969c54c990bd
yeonmi <ar77gt@daum.net>
2024-03-29 13:09:44 +0900

    feat : quizpage
    Former-commit-id: 73f62f63e802c25919ace29d387fdca5666a634c [formerly d1b6f5dc19f121dce3fedc55ad90dc241c206ef6]
    Former-commit-id: 4d4db95ae8495f07a206ee1afc2988c0ade11ca7

--- frontend/src/components/cartoon/Progressbar.tsx
+++ frontend/src/components/cartoon/Progressbar.tsx
@@ -2,8 +2,8 @@ const Progressbar = ({ value, max, onNumberClick}) => {
     return (
         <div className="flex justify-center space-x-4">
             {Array.from({ length: max }, (_, index) => (
-                <div key={index} className={`h-10 w-10 rounded-full text-white font-bold flex justify-center items-center ${
-                    index === value ? 'bg-gradient-to-r from-[#59CDE0] to-[#8F89EB]' : 'bg-[#53525f]'
+                <div key={index} className={`h-10 w-10 text-white font-bold flex justify-center items-center ${
+                    index === value ? 'bg-gradient-to-r from-[#59CDE0] to-[#8F89EB]' : 'bg-[#2D2D32]'
                 }`}
                      onClick={() => onNumberClick(index)}
                 >{index+1}</div>
