1a907b078e71516f1f967da8614b17451f12d381
Jaechan Kim <penguin_03@naver.com>
2024-03-29 10:32:18 +0900

    feat:viewer

--- frontend/src/pages/cartoon/Viewer.tsx
+++ frontend/src/pages/cartoon/Viewer.tsx
@@ -1,94 +1,73 @@
-import {useCallback, useEffect} from "react";
+import {useCallback, useEffect, useState} from "react";
 import "./hoverBox.css"
 import {useInfiniteQuery} from "@tanstack/react-query";
 import {ChapterListResponse} from "../../types/types";
 import {getChapterList} from "../../apis/cartoon.ts";
 import {useInView} from "react-intersection-observer";
 import {getImageByChapterId} from "../../apis/viewer.ts";
-import {useParams} from "react-router-dom";
-
+import {Link, useParams} from "react-router-dom";
+import client from "../../apis";
 import img from '../../assets/test.jpg';
+import ChapterList from "../../components/cartoon/ChapterList.tsx";
 
-const test = {
-    content:[
-        {
-            imageUrl:"test",
-            width:800,
-            height:1137,
-            speeches:[{
-                id:516,
-                sentence: "abcd",
-                position: {
-                    id: 516,
-                    leftTopX: 384,
-                    leftTopY: 207,
-                    rightTopX: 450,
-                    rightTopY: 207,
-                    leftBottomX: 384,
-                    leftBottomY: 292,
-                    rightBottomX: 450,
-                    rightBottomY: 292
-                }
-         }]
-        }
-    ]
-}
+// const test = {
+//     content:[
+//         {
+//             imageUrl:"test",
+//             width:800,
+//             height:1137,
+//             speeches:[{
+//                 id:516,
+//                 sentence: "abcd",
+//                 position: {
+//                     id: 516,
+//                     leftTopX: 384,
+//                     leftTopY: 207,
+//                     rightTopX: 450,
+//                     rightTopY: 207,
+//                     leftBottomX: 384,
+//                     leftBottomY: 292,
+//                     rightBottomX: 450,
+//                     rightBottomY: 292
+//                 }
+//          }]
+//         }
+//     ]
+// }
 
 const defaultSize = 5;
 
-const Viewer = () => {
-    const data = {...test};
-    // console.log("viewr page")
-    // const params = useParams()
-    // const mangaId = + params.title;
-    // const chapterId = + params.id;
-    // const {
-    //     data,
-    //     error,
-    //     fetchNextPage,
-    //     hasNextPage,
-    //     isFetching,
-    //     isFetchingNextPage,
-    //     status,
-    // } = useInfiniteQuery<ChapterListResponse>({
-    //     queryKey: ['chapter', mangaId,],
-    //     queryFn: ({pageParam = 0}) => {
-    //         // console.log("page param " + pageParam);
-    //         return getImageByChapterId({
-    //             page: + pageParam,
-    //             chapterId,
-    //             size:defaultSize,
-    //         })
-    //     },
-    //     initialPageParam: 0,
-    //     getNextPageParam: (lastPage, allPages, lastPageParam) => {
-    //         if(typeof lastPageParam !== 'number') {
-    //             return 0;
-    //         }
-    //         return lastPageParam + 1
-    //     },
-    // });
-    // const {ref, inView} = useInView({
-    //     threshold: 0,
-    //     triggerOnce: false
-    // })
-    //
-    // useEffect(() => {
-    //     if(inView){
-    //         fetchNextPage();
-    //     }
-    // }, [inView]);
 
-    // const imageRef = useCallback((node: HTMLElement) =>{
-    //     if(node !== null){
-    //         //위치 얻기 위함.....
-    //     }
-    // },[]);
+const Viewer = () => {
+    const params = useParams()
+    const mangaId = + params.title;
+    const chapterId = + params.id;
+    const [data, setData] = useState();
+    const [page, setPage] = useState();
+    const {ref, inView} = useInView({
+        threshold: 0,
+        triggerOnce: false
+    })
+
+    useEffect(() => {
+        if(inView) {
+            //chapterId: number, page: number, size: number}
+            setData({...data, getImageByChapterId({chapterId, page, size:5}})
+        }
+    }, []);
+
+    const imageRef = useCallback((node: HTMLElement) =>{
+        if(node !== null){
+            //위치 얻기 위함.....
+        }
+    },[]);
     return (
         <div className="bg-gray-600">
-            <img src={img}  className="w-[700px]" />
 
-            {/*<div ref={ref}></div>*/}
+
+
+            <div ref={ref}></div>
+            <Link to={`/quiz/new/${chapterId}`}>test</Link>
         </div>
     );
 }
