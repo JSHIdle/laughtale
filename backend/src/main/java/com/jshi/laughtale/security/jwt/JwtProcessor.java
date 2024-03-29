ae2a4fb30e3deda26a1d05c7577b3b83de4095df
chlwodyd9829 <chlwodyd9829@naver.com>
2024-03-29 15:15:08 +0900

    fix: duration 수정
    Former-commit-id: 42239058d85461a598e11109d4d4b0c8b7ebf91e [formerly b8f0c95e6abd1b9757c108f5981e9bee854951cd]
    Former-commit-id: 29d2a2471343393ae50e5173cb4649784db6dcc4

--- backend/src/main/java/com/jshi/laughtale/security/jwt/JwtProcessor.java
+++ backend/src/main/java/com/jshi/laughtale/security/jwt/JwtProcessor.java
@@ -47,7 +47,7 @@ public class JwtProcessor implements BeanPostProcessor {
         Claims claims = Jwts.claims()
                 .setSubject(email)
                 .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + Duration.ofHours(1).toNanos()));
+                .setExpiration(new Date(now.getTime() + Duration.ofHours(1).toMillis()));
         return Jwts.builder()
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
                 .setClaims(claims)
