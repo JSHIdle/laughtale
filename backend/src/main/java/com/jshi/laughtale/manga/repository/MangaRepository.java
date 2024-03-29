dd73a6d3a887fe7d8ef8be36633efdd5220844cf
chlwodyd9829 <chlwodyd9829@naver.com>
2024-03-29 10:47:52 +0900

    fix: 만화 레벨별 불러오는 기능 및 해당 레벨 만화 불러오기

--- backend/src/main/java/com/jshi/laughtale/manga/repository/MangaRepository.java
+++ backend/src/main/java/com/jshi/laughtale/manga/repository/MangaRepository.java
@@ -2,8 +2,7 @@ package com.jshi.laughtale.manga.repository;
 
 import com.jshi.laughtale.manga.domain.Manga;
 import com.jshi.laughtale.manga.dto.LevelManga;
-import com.jshi.laughtale.manga.dto.RecentManga;
-
+import jakarta.persistence.Tuple;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,8 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
-
-import jakarta.persistence.Tuple;
 
 public interface MangaRepository extends JpaRepository<Manga, Long> {
 
@@ -23,18 +20,17 @@ public interface MangaRepository extends JpaRepository<Manga, Long> {
             "ORDER BY C.chapterNo DESC " +
             "LIMIT 1")
     Optional<Integer> findLastChapterByManga(String title);
+
     Optional<Manga> findByTitle(String title);
 
 
+    @Query(value = "SELECT m.title, m.thumbnail, m.id \n"
+            + "FROM view_history v, manga m ,chapter c\n"
+            + "WHERE v.member_id = :memberId and v.chapter_id = c.id and c.manga_id = m.id\n"
+            + "GROUP BY m.id\n"
+            + "ORDER BY MAX(v.view_date) desc LIMIT 10", nativeQuery = true)
+    List<Tuple> findRecentManga(Long memberId);
 
-@Query(value = "SELECT m.title, m.thumbnail, m.id \n"
-	+ "FROM view_history v, manga m ,chapter c\n"
-	+ "WHERE v.member_id = :memberId and v.chapter_id = c.id and c.manga_id = m.id\n"
-	+ "GROUP BY m.id\n"
-	+ "ORDER BY MAX(v.view_date) desc LIMIT 10", nativeQuery = true)
-	List<Tuple> findRecentManga(Long memberId);
-
-Page<LevelManga.Response> findByLevel(int level, Pageable pageable);
-
+    Page<LevelManga.Response> findAllByLevel(int level, Pageable pageable);
 
 }
