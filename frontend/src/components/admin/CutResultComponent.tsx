89d7a2c873e18a0aba320ec62be3572d2542e474
j-yong98 <chlwodyd9829@gmail.com>
2024-03-30 21:15:10 +0900

    feat: 만화 업로드 결과 모달 기능
    Former-commit-id: 5fab4a4e19b83106ecb171f7beacfe23462a704d [formerly b7430e75f04e3e8c0d2d53507a610e2c974142a8]
    Former-commit-id: 08a436f98d5826211d9f249108acf4b10785c6ec

--- frontend/src/components/admin/CutResultComponent.tsx
+++ frontend/src/components/admin/CutResultComponent.tsx
@@ -1,43 +1,58 @@
 import {CutAnalyze} from "../../../types";
-import React, {useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import WordResult from "./WordResultComponent.tsx";
-import Pagenation from "./Pagenation.tsx";
+import Pagination from "./Pagination.tsx";
+import ImageWithIndexAndRect from "./ImageWithIndexAndRect.tsx";
+import SentenceResult from "./SentenceResultComponent.tsx";
 
 export default function CutResult(props: CutAnalyze) {
-    const sentenceCur = useRef(1);
-    const sentenceLength = useRef(props.sentence.length);
-    const wordCur = useRef();
+    const speechLength = useRef(props.sentence.length);
+    const [speechCur, setSpeechCur] = useState(1);
+
+    const [speechContent, setSpeechContent] = useState(props.sentence[0] ?? null);
     const wordLength = useRef(props.words.length);
+    const [wordCur, setWordCur] = useState(1);
+
+    const [wordContent, setWordContent] = useState(props.words[0] ?? null);
+
+    const handleWordCur = (value: number) => {
+        setWordCur(wordCur + value);
+        setWordContent(props.words[wordCur + value - 1]);
+    }
+
+    const handleSpeechCur = (value: number) => {
+        setSpeechCur(speechCur + value);
+        setSpeechContent(props.sentence[speechCur + value - 1]);
+    }
+
     return (
         <div className="grid grid-cols-2 gap-5">
-            <div className="border-2 border-black">
-                <img id="cutImage" src={props.imageUrl} alt=""/>
-            </div>
-            <div className="flex flex-col ">
-                <p className="text-3xl font-bold p-4">나온 문장들</p>
+            <ImageWithIndexAndRect index={speechCur} src={props.imageUrl} boxCoordinates={{
+                x: speechContent.positionBasic.leftBottomX,
+                y: speechContent.positionBasic.leftBottomY,
+                width: speechContent.positionBasic.rightTopX - speechContent.positionBasic.leftBottomX,
+                height: speechContent.positionBasic.rightTopY - speechContent.positionBasic.leftBottomY
+            }}/>
+            <div className="flex flex-col border rounded">
+                <p className="text-3xl font-bold p-4 border-b   ">문장</p>
+                <div className="p-6 shadow rounded">
+                    <div className="text-center mb-5">
+                        <SentenceResult sentence={speechContent.sentence}/>
+                    </div>
+                </div>
+                <Pagination length={speechLength.current} cur={speechCur} setCur={handleSpeechCur}></Pagination>
+                <p className="text-3xl font-bold p-4 border rounded">단어</p>
                 <div className="relative flex-1">
                     <div
-                        className="border border-2 border-amber-600 absolute top-0 bottom-0 left-0 right-0 overflow-y-scroll">
+                        className="absolute top-0 bottom-0 left-0 right-0 overflow-y-scroll">
                         <div className="absolute top-0 bottom-0 left-0 right-0 p-4">
                             <div>
-                                {props.sentence.map((sentence) => <p>{sentence}</p>)}
+                                {<WordResult {...wordContent} />}
                             </div>
                         </div>
                     </div>
                 </div>
-                <Pagenation cur={sentenceCur.current} length={sentenceLength.current}></Pagenation>
-                <p className="text-3xl font-bold p-4">나온 단어들</p>
-                <div className="relative flex-1">
-                    <div
-                        className="border border-2 border-amber-600 absolute top-0 bottom-0 left-0 right-0 overflow-y-scroll">
-                        <div className="absolute top-0 bottom-0 left-0 right-0 p-4">
-                            <div>
-                                {props.words.map((d) => <WordResult {...d} />)}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <Pagenation cur={wordCur.current} length={wordLength.current}></Pagenation>
+                <Pagination length={wordLength.current} cur={wordCur} setCur={handleWordCur}></Pagination>
             </div>
         </div>
     );
