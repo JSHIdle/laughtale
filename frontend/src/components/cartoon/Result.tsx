7058e23c0deeca77aff3ab84eca22b5ecd1db4dd
yeonmi <ar77gt@daum.net>
2024-04-01 01:27:37 +0900

    feat:
    Former-commit-id: b30d3ccc1e27989f63525e105f1c19fa791d65bd [formerly a24eec650e3aa10b48de693b74bddf9ec293b272]
    Former-commit-id: 024e26a9041838888a0e599e4b5d146b2efd4749

--- frontend/src/components/cartoon/Result.tsx
+++ frontend/src/components/cartoon/Result.tsx
@@ -15,34 +15,34 @@ const Result = () => {
     const {chapterId} = useParams();
     const nextchapter = +chapterId+1;
 
-    return (<div className="flex flex-col bg-[#121212]" style={{ height: 'calc(100vh * 1.1111)' }}>
-        <Header/>
-        <div className="flex justify-center items-center">
-            <div className="max-w-[1180px] mt-12">
-                <div className="flex flex-col justify-center items-center">
-                    <div
-                        className="w-[400px] text-[20px] font-bold text-white flex justify-center items-center">
-                        퀴즈 풀이 결과
-                    </div>
-                    <div className="text-white mt-6">
-                        <div>
-                            점수 : {20 * correctAnswersCount}점
+    return (
+        <div className="flex flex-col h-screen bg-[#121212]" style={{ height: 'calc(100vh * 1.1111)' }}>
+            <Header/>
+            <div className="flex-grow flex justify-center items-center">
+                <div className="w-full max-w-[1180px] h-[600px]">
+                    <div className="flex flex-col justify-center items-center p-12">
+                        <div className="w-[400px] text-[20px] font-bold text-white flex justify-center items-center p-6">
+                            퀴즈 풀이 결과
+                        </div>
+                        <div className="text-white mt-6 p-6 text-5xl">
+                            <div>
+                                {20 * correctAnswersCount}점
+                            </div>
                         </div>
                     </div>
-
-                </div>
-                <div className="p-12">
-                    <ResultWord slides={slides}/>
-                </div>
-                <div className="flex justify-center text-white">
-                    <Link to={`${prefix}/viewer/${nextchapter}`} replace
-                          className="p-6 text-2xl text-white bg-[#2D2D32] brightness-100 hover:brightness-115 rounded-3xl ">
-                        다음 회차 보기
-                    </Link>
+                    <div className="z-10 flex justify-center items-center">
+                        <ResultWord slides={slides}/>
+                    </div>
+                    <div className="flex text-white justify-center items-center mt-4">
+                        <Link to={`${prefix}/viewer/${nextchapter}`} replace
+                              className="z-20 p-6 text-2xl text-white bg-[#2D2D32] brightness-100 hover:brightness-125 rounded-3xl">
+                            다음 회차 보기
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>);
+    );
 }
 
 export default Result;
