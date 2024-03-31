1ffb9d5342d8a00dd2890a64c264ae1029bec4b6
seunghw2 <seunghw2@gmail.com>
2024-03-31 16:46:24 +0900

    feat : Manga Level 설정하는 기능 추가
    Former-commit-id: 5338cba64aebf13e57f96e87008591bbc1ce35a6 [formerly a84d8c022b302686527290aaac04581196bf3b59]
    Former-commit-id: 09c330213cdf6f1db01e949244946764f9dee627

--- backend/src/main/java/com/jshi/laughtale/manga/repository/MangaRepository.java
+++ backend/src/main/java/com/jshi/laughtale/manga/repository/MangaRepository.java
@@ -3,34 +3,36 @@ package com.jshi.laughtale.manga.repository;
 import com.jshi.laughtale.manga.domain.Manga;
 import com.jshi.laughtale.manga.dto.LevelManga;
 import jakarta.persistence.Tuple;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.repository.query.Param;
 
 public interface MangaRepository extends JpaRepository<Manga, Long> {
 
     @Query(value = "SELECT C.chapterNo " +
-            "FROM Manga AS M " +
-            "JOIN Chapter AS C ON C.manga.id = M.id " +
-            "WHERE M.title = :title " +
-            "ORDER BY C.chapterNo DESC " +
-            "LIMIT 1")
+        "FROM Manga AS M " +
+        "JOIN Chapter AS C ON C.manga.id = M.id " +
+        "WHERE M.title = :title " +
+        "ORDER BY C.chapterNo DESC " +
+        "LIMIT 1")
     Optional<Integer> findLastChapterByManga(String title);
 
     Optional<Manga> findByTitle(String title);
 
 
     @Query(value = "SELECT m.title, m.thumbnail, m.id \n"
-            + "FROM view_history v, manga m ,chapter c\n"
-            + "WHERE v.member_id = :memberId and v.chapter_id = c.id and c.manga_id = m.id\n"
-            + "GROUP BY m.id\n"
-            + "ORDER BY MAX(v.view_date) desc LIMIT 10", nativeQuery = true)
+        + "FROM view_history v, manga m ,chapter c\n"
+        + "WHERE v.member_id = :memberId and v.chapter_id = c.id and c.manga_id = m.id\n"
+        + "GROUP BY m.id\n"
+        + "ORDER BY MAX(v.view_date) desc LIMIT 10", nativeQuery = true)
     List<Tuple> findRecentManga(Long memberId);
 
     Page<LevelManga.Response> findAllByLevel(int level, Pageable pageable);
 
+    @Query("SELECT m FROM Manga m JOIN FETCH m.chapter c WHERE m.id = :id")
+    Manga findMangaByIdFetchJoinChapter(@Param("id") Long id);
 }
--- backend/src/main/java/com/jshi/laughtale/manga/service/MangaService.java
+++ backend/src/main/java/com/jshi/laughtale/manga/service/MangaService.java
@@ -1,5 +1,6 @@
 package com.jshi.laughtale.manga.service;
 
+import com.jshi.laughtale.chapter.domain.Chapter;
 import com.jshi.laughtale.manga.domain.Manga;
 import com.jshi.laughtale.manga.dto.LevelManga;
 import com.jshi.laughtale.manga.dto.MangaAnalyze;
@@ -9,22 +10,19 @@ import com.jshi.laughtale.manga.mapper.MangaMapper;
 import com.jshi.laughtale.manga.repository.MangaRepository;
 import com.jshi.laughtale.utils.DataRequest;
 import com.jshi.laughtale.utils.FileUtils;
-
 import jakarta.persistence.Tuple;
 import jakarta.transaction.Transactional;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Service
@@ -32,61 +30,93 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class MangaService {
 
-	private final MangaRepository mangaRepository;
-	private final MangaParser mangaParser;
+    private final MangaRepository mangaRepository;
+    private final MangaParser mangaParser;
 
-	public MangaAnalyze.Response upload(MultipartFile thumbnail, MangaUpload.Request manga, List<MultipartFile> files) throws
-		IOException {
-		String thumbnailPath = FileUtils.save(thumbnail, manga.getTitle(), "thumbnail");
-		log.info("썸네일 저장 완료");
-		int last = mangaRepository.findLastChapterByManga(manga.getTitle()).orElse(0) + 1;
-		log.info("챕터 번호 불러오기 성공");
+    public MangaAnalyze.Response upload(MultipartFile thumbnail, MangaUpload.Request manga,
+        List<MultipartFile> files) throws
+        IOException {
+        String thumbnailPath = FileUtils.save(thumbnail, manga.getTitle(), "thumbnail");
+        log.info("썸네일 저장 완료");
+        int last = mangaRepository.findLastChapterByManga(manga.getTitle()).orElse(0) + 1;
+        log.info("챕터 번호 불러오기 성공");
 
-		List<String> names = new ArrayList<>();
-		for (MultipartFile file : files) {
-			String filename = FileUtils.save(file, manga.getTitle(), String.valueOf(last));
-			names.add(filename);
-		}
-		log.info("컷 이미지 저장 성공");
+        List<String> names = new ArrayList<>();
+        for (MultipartFile file : files) {
+            String filename = FileUtils.save(file, manga.getTitle(), String.valueOf(last));
+            names.add(filename);
+        }
+        log.info("컷 이미지 저장 성공");
 
+        MangaAnalyze.Request analyzeRequest = MangaMapper.toAnalyze(thumbnailPath, manga, last,
+            names);
+        Map result = DataRequest.analyze(analyzeRequest);
+        log.info("분석 완료");
 
-		MangaAnalyze.Request analyzeRequest = MangaMapper.toAnalyze(thumbnailPath, manga, last, names);
-		Map result = DataRequest.analyze(analyzeRequest);
-		log.info("분석 완료");
+        Manga m = mangaRepository.findByTitle(manga.getTitle())
+            .orElse(MangaMapper.analyzeToEntity(analyzeRequest));
 
-		Manga m = mangaRepository.findByTitle(manga.getTitle())
-			.orElse(MangaMapper.analyzeToEntity(analyzeRequest));
+        mangaRepository.save(m);
+        MangaAnalyze.Response response = mangaParser.parser(m, result, last);
+        m.update();
+        log.info("파싱 완료");
+        return response;
+    }
 
-		mangaRepository.save(m);
-		MangaAnalyze.Response response = mangaParser.parser(m, result, last);
-		m.update();
-		log.info("파싱 완료");
-		return response;
-	}
+    public List<RecentManga.Response> getRecentManga(Long memberId) {
+        List<Tuple> tupleList = mangaRepository.findRecentManga(memberId);
+        List<RecentManga.Response> response = new ArrayList<>();
+        for (Tuple tuple : tupleList) {
+            response.add(RecentManga.Response.builder()
+                .id(tuple.get("id", Long.class))
+                .thumbnail(tuple.get("thumbnail", String.class))
+                .title(tuple.get("title", String.class))
+                .build());
+        }
+        return response;
+    }
 
-	public List<RecentManga.Response> getRecentManga(Long memberId) {
-		List<Tuple> tupleList = mangaRepository.findRecentManga(memberId);
-		List<RecentManga.Response> response = new ArrayList<>();
-		for (Tuple tuple : tupleList) {
-			response.add(RecentManga.Response.builder()
-				.id(tuple.get("id", Long.class))
-				.thumbnail(tuple.get("thumbnail", String.class))
-				.title(tuple.get("title", String.class))
-				.build());
-		}
-		return response;
-	}
+    public Page<LevelManga.Response> getLevelManga(int level, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return mangaRepository.findAllByLevel(level, pageable);
+    }
 
-	public Page<LevelManga.Response> getLevelManga(int level, int page, int size) {
-		Pageable pageable = PageRequest.of(page, size);
-		return mangaRepository.findAllByLevel(level, pageable);
-	}
+    public Manga getMangaInfo(Long id) {
+        return mangaRepository.findById(id).orElseThrow();
+    }
 
-	public Manga getMangaInfo(Long id) {
-		return mangaRepository.findById(id).orElseThrow();
-	}
+    public Manga findById(Long mangaId) {
+        return mangaRepository.findById(mangaId).orElseThrow();
+    }
 
-	public Manga findById(Long mangaId) {
-		return mangaRepository.findById(mangaId).orElseThrow();
-	}
+    public int calculateMangaLevel(long id) {
+        Manga manga = mangaRepository.findMangaByIdFetchJoinChapter(id);
+
+        int totalSum = 0;
+        int totalCnt = manga.getChapter().size();
+
+        for (Chapter chapter : manga.getChapter()) {
+            totalSum += chapter.getLevel();
+        }
+
+        return averageToLevel(totalSum, totalCnt);
+    }
+
+    public int averageToLevel(int totalSum, int totalCnt) {
+        double avg = (double) totalSum / totalCnt;
+
+        if (avg <= 1.67) {
+            return 1;
+        }
+        if (avg <= 1.73) {
+            return 2;
+        }
+        if (avg <= 1.8) {
+            return 3;
+        }
+        if (avg <= 1.9) {
+            return 4;
+        }
+        return 5;
+    }
 }
