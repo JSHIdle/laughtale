87181aca2e3f2bc9d574ea548a4a34537f31f43f
jinooyang <jinooyang@gmail.com>
2024-04-01 01:16:45 +0900

    design : new main page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,50 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],  theme: {
-    extend: {
-      zIndex: {
-        '1000': 1000,
-      }},
-  },
-  plugins: [require('tailwind-scrollbar-hide')],
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ], theme: {
+        extend: {
+            keyframes: {
+                sliderA: {
+                    '0%': {
+                        transform: 'translateX(0px)',
+                        backgroundColor: '#121212',opacity : 0.2
+                    },
+                    '100%': {
+                        transform: 'translateX(-1000px)',
+                        backgroundColor: '#121212',opacity : 0.2
+                    }
+                },
+                sliderB: {
+                    '0%': {
+                        transform: 'translateX(-1000px)',
+                        backgroundColor: '#121212',opacity : 0.2
+                    },
+                    '100%': {
+                        transform: 'translateX(0px)',
+                        backgroundColor: '#121212',opacity : 0.2
+                    }
+                },
+                sliderC: {
+                    '0%': {
+                        transform: 'translateX(-150px)',
+                        backgroundColor: '#121212',opacity : 0.2
+                    },
+                    '100%': {
+                        transform: 'translateX(-1150px)',
+                        backgroundColor: '#121212',opacity : 0.2
+                    }
+                },
+
+            },
+            animation: {
+                sliderA: 'sliderA 120s linear infinite',
+                sliderB: 'sliderB 120s linear infinite',
+                sliderC: 'sliderC 120s linear infinite',
+            }
+        },
+    },
+    plugins: [require('tailwind-scrollbar-hide')],
 }
+
