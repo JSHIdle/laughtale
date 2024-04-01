47bf09c9129f8b654678194d42e624258786f63e
dohyeong <ehgud619@naver.com>
2024-04-01 19:12:57 +0900

    feature:망각곡선차트 수정
    Former-commit-id: 0a7db4761cad26d4c8a0afe1c054c396f063cdc1 [formerly 7d1f0e2665b53ba998f2c732a52b9f3fb95106c7]
    Former-commit-id: 7b4caa197e8b9aef823cfcf9e632b91f37f1f878

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],  theme: {
     extend: {
+      colors: {
+        'custom-bronze': 'rgb(173,86,0)',
+        'custom-silver': 'rgb(67,95,122)',
+        'custom-gold': 'rgb(236,154,0)',
+        'custom-platinum': 'rgb(39,226,164)',
+        'custom-diamond': 'rgb(0,180,252)',
+        'custom-bronze-dark': 'rgba(173,86,0,0.8)',
+        'custom-silver-dark': 'rgba(67,95,122,0.8)',
+        'custom-gold-dark': 'rgba(236,154,0,0.8)',
+        'custom-platinum-dark': 'rgba(39,226,164,0.8)',
+        'custom-diamond-dark': 'rgba(0,180,252,0.8)',
+      },
       zIndex: {
         '1000': 1000,
       },
@@ -15,3 +27,4 @@ export default {
   },
   plugins: [require('tailwind-scrollbar-hide')],
 }
+
