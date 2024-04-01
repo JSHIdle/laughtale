74814c3e2f3bf754f010a522823571f9e0a378fe
jinooyang <jinooyang@gmail.com>
2024-04-02 08:35:12 +0900

    fix : after login header

--- frontend/src/components/common/AfterLoginButton.tsx
+++ frontend/src/components/common/AfterLoginButton.tsx
@@ -3,20 +3,22 @@ import {useAuth} from "../../stores/useAuth.ts";
 import {useCallback} from "react";
 
 export default function AfterLoginButton() {
-  const navigate = useNavigate();
-  const clear  = useAuth((state) => state.clear);
-  const logout =() => {
-    clear();
-    navigate("/home");
-  }
-  return (
-      <>
-      <div className="mr-3 text-3xl hover:text-gray-800">
-        <NavLink to="/mypage">マイページ : 마이페이지</NavLink>
-      </div>
+    const navigate = useNavigate();
+    const clear = useAuth((state) => state.clear);
+    const logout = () => {
+        clear();
+        navigate("/home");
+    }
+    return (
+        <>
+            <div className="flex justify-end">
+                <div className="mr-10 text-3xl hover:text-gray-800 text-white">
+                    <NavLink to="/mypage">マイページ : 마이페이지</NavLink>
+                </div>
 
-      <div className="text-3xl" onClick={() => logout()}>
-          ログアウト : 로그아웃
-      </div>
-  </>)
+                <div className="text-3xl text-white hover:text-gray-800 hover:cursor-pointer" onClick={() => logout()}>
+                    ログアウト : 로그아웃
+                </div>
+            </div>
+        </>)
 }
