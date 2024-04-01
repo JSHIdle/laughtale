b64576c53970ae85cdfe13937fa8584bed2f2b39
yeonmi <ar77gt@daum.net>
2024-04-01 17:38:29 +0900

    feat :mouse
    Former-commit-id: 0f4cdfc8e1121f794f9726d20e54b620858b8c58 [formerly 80b8ed4cc833014a2d0b80479da5679b7602bfa2]
    Former-commit-id: f000ba9094e34d12f475ee689e8b4636987d3622

--- frontend/src/components/cartoon/Progressbar.tsx
+++ frontend/src/components/cartoon/Progressbar.tsx
@@ -2,11 +2,11 @@ const Progressbar = ({ value, max, onNumberClick}) => {
     return (
         <div className="flex justify-center space-x-4">
             {Array.from({ length: max }, (_, index) => (
-                <div key={index} className={`h-10 w-10 text-black font-bold flex justify-center items-center ${
+                <button key={index} className={`h-10 w-10 text-black font-bold flex justify-center items-center ${
                     index === value ? 'bg-gradient-to-r from-[#59CDE0] to-[#8F89EB]' : 'border-2 border-[#4EDBDE]'
                 }`}
                      onClick={() => onNumberClick(index)}
-                >{index+1}</div>
+                >{index+1}</button>
             ))}
 
         </div>
