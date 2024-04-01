e010031317fec98acd19b990456bacf0a44a661f
jinooyang <jinooyang@gmail.com>
2024-04-01 15:59:21 +0900

    feat : api/word-data/word-cloud
    Former-commit-id: c9f26a1b75bcc4d31142678f5a3ba58a0be06062 [formerly dd90f6bf51f3b52d64dc4a8fb2993fbcadf6ad12]
    Former-commit-id: 734955245d557c7057854948884410088a281763

--- backend/src/main/java/com/jshi/laughtale/worddata/controller/WordDataController.java
+++ backend/src/main/java/com/jshi/laughtale/worddata/controller/WordDataController.java
@@ -1,8 +1,13 @@
 package com.jshi.laughtale.worddata.controller;
 
+import com.jshi.laughtale.worddata.dto.WordCloudData;
 import com.jshi.laughtale.worddata.dto.WordDataDetail;
 import com.jshi.laughtale.worddata.service.WordDataService;
+
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,15 +21,20 @@ import java.util.List;
 @RequestMapping("/api/word-data")
 public class WordDataController {
 
-    private final WordDataService wordDataService;
+	private final WordDataService wordDataService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<WordDataDetail.Response> getWordData(@PathVariable Long id) {
-        return ResponseEntity.ok(wordDataService.loadWordDataDetail(id));
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<WordDataDetail.Response> getWordData(@PathVariable Long id) {
+		return ResponseEntity.ok(wordDataService.loadWordDataDetail(id));
+	}
 
-    @GetMapping("/speech/{speechId}")
-    public ResponseEntity<List<WordDataDetail.Response>> getWords(@PathVariable Long speechId) {
-        return ResponseEntity.ok(wordDataService.loadWordDataDetailWithoutSpeech(speechId));
-    }
+	@GetMapping("/speech/{speechId}")
+	public ResponseEntity<List<WordDataDetail.Response>> getWords(@PathVariable Long speechId) {
+		return ResponseEntity.ok(wordDataService.loadWordDataDetailWithoutSpeech(speechId));
+	}
+
+	@GetMapping("/word-cloud")
+	public ResponseEntity<List<WordCloudData.Response>> getWordCloudData() {
+		return ResponseEntity.ok(wordDataService.loadWordCloudData());
+	}
 }
