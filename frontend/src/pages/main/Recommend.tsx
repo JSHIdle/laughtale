d5207ba002496ca0c0dbcbc56ede7869fd2cfa06
yeonmi <ar77gt@daum.net>
2024-04-02 08:14:07 +0900

    design : mainpage

--- frontend/src/pages/main/Recommend.tsx
+++ frontend/src/pages/main/Recommend.tsx
@@ -1,35 +1,34 @@
 import React, { useEffect, useState } from 'react';
-
 import Header from '../../components/common/Header';
 import CartoonGrid from "../../components/main/CartoonGrid";
-
-
 import RecentWord from '../../components/main/RecentWord.tsx';
 import '../../index.css'
+import NavBar from "./NavBar.tsx";
 
 const Recommend = () => {
-
-    //레벨별 만화 4개씩 불러오기
+    const [selectedLevel, setSelectedLevel] = useState(1);
+    const [filteredCartoons, setFilteredCartoons] = useState([]);
     const [cartoons, setCartoons] = useState([]);
     useEffect(() => {
         // API 요청을 통해 만화 데이터를 가져옵니다.
         const fetchCartoons = async () => {
             const response = await fetch('https://j10a705.p.ssafy.io/api/manga');
             const data = await response.json();
-
-            console.log("여기 옴 ??")
-            console.log(data)
-            // 상태에 응답 데이터를 저장합니다.
+            console.log("api전체 데이터",data)
             setCartoons(data);
         };
         fetchCartoons();
     }, []);
-    // 데이터가 없으면 로딩 메시지를 표시합니다.
-    // if (cartoons.length === 0) {
-    //     return <div>Loading...</div>;
-    // }
 
-
+    useEffect(() => {
+        if(cartoons.length >= selectedLevel && cartoons[selectedLevel - 1] !== undefined){
+            const levelCartoons = cartoons[selectedLevel - 1] || []; // cartoons 속성이 있는지 확인하고, 없으면 빈 배열 사용
+            const updatedFilteredCartoons = levelCartoons.slice(0, 5);
+            setFilteredCartoons(updatedFilteredCartoons);
+        } else {
+            setFilteredCartoons([]);
+        }
+    }, [cartoons, selectedLevel]);
 
     return (
         <div className="bg-[#ffffff] min-h-screen text-black">
@@ -37,9 +36,11 @@ const Recommend = () => {
             <div className="mx-auto recent_wrap" style={{maxWidth: '950px'}}>
             <RecentWord/>
             </div>
-            {cartoons.map((cartoonData, index) => (
-                <CartoonGrid key={index} cartoons={cartoonData} level={index + 1} />
-            ))}
+            <NavBar selectedLevel={selectedLevel} onSelectLevel={setSelectedLevel} />
+            <CartoonGrid cartoons={filteredCartoons} level={selectedLevel} />
+            {/*{cartoons.map((cartoonData, index) => (*/}
+            {/*    <CartoonGrid key={index} cartoons={cartoonData} level={index + 1} />*/}
+            {/*))}*/}
         </div>
     );
 }
