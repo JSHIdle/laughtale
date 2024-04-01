529bc5ac139fb2c173a47afa5e7effa2033feae4
jinooyang <jinooyang@gmail.com>
2024-04-01 23:30:03 +0900

    design : header (added japanese)
    Former-commit-id: 34aa96c284f90ede0350d05f4c3c70009e064de2 [formerly d39e48ead17fca118600e3b5149831f90fdf3866]
    Former-commit-id: 299747d5ad8db1af6c47581f47c0342f754389fe

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import Logo from '../../assets/logo.png';
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import AuthButton from "./AuthButton.tsx";
 
 const Header = () => {
@@ -19,14 +19,22 @@ const Header = () => {
     }, []);
 
     return (
-        <div className={`bg-[#73ABE5] flex justify-between items-center px-10 ${isScrolled ? 'fixed top-0 left-0 right-0 z-50' : 'relative'}`}>
-            <Link to={`/admin`} className="text-lg text-white hover:text-gray-800">관리자페이지</Link>
-            <div className="flex justify-center flex-grow">
-                <Link to="/main">
-                    <img src={Logo} alt="Logo" height="200" width="170"></img>
-                </Link>
+        <div className={` ${isScrolled ? 'fixed top-0 left-0 right-0 z-50' : 'relative'}`}>
+            <div className="bg-[#73ABE5] flex justify-center items-center px-10 relative">
+                <div className="absolute left-10 items-center">
+                    <Link to={`/admin`} className="text-3xl text-white hover:text-gray-800">
+                        관리자페이지
+                    </Link>
+                </div>
+                <div className="-translate-y-2 center">
+                    <Link to="/main">
+                        <img src={Logo} alt="Logo" height="200" width="170"></img>
+                    </Link>
+                </div>
+                <div className="absolute right-10">
+                    <AuthButton/>
+                </div>
             </div>
-            <AuthButton/>
         </div>
     )
 }
