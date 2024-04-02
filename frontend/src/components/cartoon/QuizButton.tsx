10b6e0b3b115e7526588e56f4a0b8eff671e9c85
seunghw2 <seunghw2@gmail.com>
2024-04-02 20:36:42 +0900

    feat : viewer에서 quiz, 이전 화, 다음 화로 넘어가는 버튼 구현

---
+++ frontend/src/components/cartoon/QuizButton.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from "@material-tailwind/react";
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'; // 아이콘 추가
+
+type QuizButtonProps = {
+    chapterId: number;
+    mangaId: number;
+};
+
+const QuizButton: React.FC<QuizButtonProps> = ({ chapterId, mangaId }) => {
+    const navigate = useNavigate();
+
+    const handlePreviousClick = () => {
+        navigate(`/cartoon/${mangaId}/viewer/${chapterId - 1}`);
+    };
+
+    const handleNextClick = () => {
+        navigate(`/cartoon/${mangaId}/viewer/${chapterId + 1}`);
+    };
+
+    const handleQuizButtonClick = () => {
+        navigate(`/quiz/new/${chapterId}/cnt`);
+    };
+
+    return (
+        <div className="flex justify-center items-center space-x-4 p-4 m-10 rounded shadow">
+            <MdChevronLeft
+                className="cursor-pointer text-5xl"
+                onClick={handlePreviousClick}
+            />
+            <Button
+                color="blue"
+                onClick={handleQuizButtonClick}
+                className="text-5xl py-6 px-12"
+            >
+                퀴즈 풀면서 단어 학습하기!
+            </Button>
+            <MdChevronRight
+                className="cursor-pointer text-5xl"
+                onClick={handleNextClick}
+            />
+        </div>
+    );
+};
+
+export default QuizButton;
