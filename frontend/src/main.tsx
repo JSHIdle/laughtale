373bace2fbb8f65f0e3bc7bf235a217c7df9e998
Jaechan Kim <penguin_03@naver.com>
2024-04-02 14:41:04 +0900

    feat:auto login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,21 +4,7 @@ import router from "./routes";
 import "./index.css"
 import './styles/SliderStyles.css';
 import './styles/fontstyle.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClient } from '@tanstack/react-query';
+import App from "./App.tsx";
 
-const queryClient = new QueryClient({
-  defaultOptions:{
-    queries:{
-
-    }
-  }
-});
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-
-);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App/>);
