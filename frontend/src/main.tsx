716dfd5b73c2e21cbc28a037b7cb48910cb4a3cc
jinooyang <jinooyang@gmail.com>
2024-04-02 11:02:00 +0900

    feat : font

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import "./index.css"
 import './styles/SliderStyles.css';
+import './styles/fontstyle.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
