faa06a8fb182d658d88e64e096e2d9a86bc46911
Jaechan Kim <penguin_03@naver.com>
2024-04-03 19:01:38 +0900

    feat/everything merge
    Former-commit-id: 74ca44000d4c97b369eace3d5808e19aaf0378ea [formerly d3eae607b97ba5bd7439be7161967b19c33fbd2c]
    Former-commit-id: a7ef6457390224f97674203ab57379892191b622

--- frontend/src/components/cartoon/ChapterList.tsx
+++ frontend/src/components/cartoon/ChapterList.tsx
@@ -1,6 +1,11 @@
 import ChapterItem from "./ChapterItem.tsx";
 import {useQueryClient} from "@tanstack/react-query";
+<<<<<<< HEAD
 import {ChapterHistory} from "../../../types";
+=======
+import {Cartoon} from "../../types/types";
+import {ChapterHistory} from "../../pages/cartoon";
+>>>>>>> 6e3d82f (feat/everything)
 // "chapterId": 7,
 //   "chapterNo": 0,
 //   "thumbnail": "/home/ubuntu/images/ジャガイモ農家の村娘、剣神と謳われるまで。/1/ジャガイモ農家の村娘、剣神と謳われるまで。--4-1.jpg",
@@ -20,5 +25,10 @@ type Props = {
 export default function ChapterList(props: Props){
   const queryClient = useQueryClient();
   const cache = queryClient.getQueryData<ChapterHistory>(["mangaHistory", props.mangaId]);
+<<<<<<< HEAD
+=======
+  console.log("cache", cache);
+
+>>>>>>> 6e3d82f (feat/everything)
   return props.content.map((item) => <ChapterItem {...item} mangaId={props.mangaId} title={props.title} viewed={cache.chaptersViewed.includes(item.chapterId)} />)
 }
