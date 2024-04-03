f23017666da81fd3f2390ea0713a88b0f79aabbe
jinooyang <jinooyang@gmail.com>
2024-04-03 17:42:46 +0900

    design : header blue
    Former-commit-id: a1a9155deb3df02b4b055b1dfdc98a07936044f6 [formerly 0730cf2b761edff04110ac7881e3446bd5674424]
    Former-commit-id: e365709d19ad738e1f0df4ca72987cd730d33433

--- frontend/src/pages/mypage/word/Index.tsx
+++ frontend/src/pages/mypage/word/Index.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import CustomSlider from '../../../components/mypage/CustomSlider.tsx';
-import Header from "../../../components/common/Header.tsx";
+import BlueHeader from "../../../components/common/Header.tsx";
 import {useQuery} from "@tanstack/react-query";
 import getWordInfo from "../../../components/mypage/getWordInfo.tsx";
 import { useParams } from 'react-router-dom';
@@ -22,7 +22,7 @@ const Index = () => {
 
   return (
       <div className="flex flex-col bg-[#ffffff]" style={{ height: 'calc(100vh * 1.1111)' }}>
-        <div><Header/></div>
+        <div><BlueHeader/></div>
         <div className="flex justify-center items-center">
           <div className="max-w-[1180px] mt-12">
             <div className="p-12">
