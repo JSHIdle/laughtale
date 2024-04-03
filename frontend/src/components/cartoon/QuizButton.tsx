482e5e6d3b2b5be70a504ec5bccf1aa3c890f779
Jaechan Kim <penguin_03@naver.com>
2024-04-03 12:48:24 +0900

    fix: quiz button error

--- frontend/src/components/cartoon/QuizButton.tsx
+++ frontend/src/components/cartoon/QuizButton.tsx
@@ -1,46 +1,56 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from "@material-tailwind/react";
-import { MdChevronLeft, MdChevronRight } from 'react-icons/md'; // 아이콘 추가
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import {useQuery} from "@tanstack/react-query";
+import {get} from "../../apis";
 
 type QuizButtonProps = {
     chapterId: number;
     mangaId: number;
 };
+type Page = {
+  nextPage:number;
+  prevPage:number;
+}
 
 const QuizButton: React.FC<QuizButtonProps> = ({ chapterId, mangaId }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const {data, isLoading, isFetching} = useQuery<Page>({
+    queryKey: ["pageNextPrev", chapterId],
+    queryFn: () => get<Page>(`/chapter/${chapterId}`)
+  })
 
-    const handlePreviousClick = () => {
-        navigate(`/cartoon/${mangaId}/viewer/${chapterId - 1}`);
-    };
+  const handlePreviousClick = () => {
+    navigate(`/cartoon/${mangaId}/viewer/${data.prevPage}`);
+  };
 
-    const handleNextClick = () => {
-        navigate(`/cartoon/${mangaId}/viewer/${chapterId + 1}`);
-    };
+  const handleNextClick = () => {
+    navigate(`/cartoon/${mangaId}/viewer/${data.nextPage}`);
+  };
 
-    const handleQuizButtonClick = () => {
-        navigate(`/quiz/new/${chapterId}/cnt`);
-    };
-
-    return (
-        <div className="flex justify-center items-center space-x-4 p-4 m-10 rounded shadow">
-            <MdChevronLeft
-                className="cursor-pointer text-5xl"
-                onClick={handlePreviousClick}
-            />
-            <button
-                onClick={handleQuizButtonClick}
-                className="text-5xl py-6 px-12 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded-3xl"
-            >
-                퀴즈 풀면서 단어 학습하기!
-            </button>
-            <MdChevronRight
-                className="cursor-pointer text-5xl"
-                onClick={handleNextClick}
-            />
-        </div>
-    );
+  const handleQuizButtonClick = () => {
+    navigate(`/quiz/new/${chapterId}/cnt`);
+  };
+  console.log(data);
+  return data ? <div className="flex justify-center items-center space-x-4 p-4 m-10 rounded shadow">
+      {data.prevPage && <MdChevronLeft
+          className="cursor-pointer text-5xl"
+          onClick={handlePreviousClick}
+      />}
+      <button
+          onClick={handleQuizButtonClick}
+          className="text-5xl py-6 px-12 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded-3xl"
+      >
+          퀴즈 풀면서 단어 학습하기!
+      </button>
+    {
+      data.nextPage && <MdChevronRight
+            className="cursor-pointer text-5xl"
+            onClick={handleNextClick}
+        />
+    }
+  </div> : <></>
 };
 
 export default QuizButton;
