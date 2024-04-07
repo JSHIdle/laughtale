07370bc82d1e6741aea8af9c1e269910756ad433
chlwodyd9829 <chlwodyd9829@naver.com>
2024-04-07 20:13:37 +0900

    fix: 단어 조회 시 우선순위 변경
    Former-commit-id: ac8509949e8c099fce4c6220c6493293f6645e88 [formerly 71a82296aea69e72e4e35e5b47a115d66a2fabd4]
    Former-commit-id: 5f0d7baa9cebba8985da66774ab72ddf666adaae

--- backend/src/main/java/com/jshi/laughtale/worddata/service/WordDataService.java
+++ backend/src/main/java/com/jshi/laughtale/worddata/service/WordDataService.java
@@ -1,6 +1,5 @@
 package com.jshi.laughtale.worddata.service;
 
-import com.jshi.laughtale.chapter.mapper.ChapterMapper;
 import com.jshi.laughtale.speech.dto.SpeechBasic;
 import com.jshi.laughtale.speech.service.SpeechService;
 import com.jshi.laughtale.worddata.domain.WordData;
@@ -11,19 +10,15 @@ import com.jshi.laughtale.worddata.mapper.WordDataMapper;
 import com.jshi.laughtale.worddata.repository.WordDataRepository;
 import com.jshi.laughtale.wordlist.domain.WordList;
 import com.jshi.laughtale.wordlist.service.WordListService;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
 
 @Slf4j
 @Service
@@ -31,56 +26,70 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class WordDataService {
 
-	private final WordListService wordListService;
-	private final SpeechService speechService;
-	private final WordDataRepository wordDataRepository;
+    private final WordListService wordListService;
+    private final SpeechService speechService;
+    private final WordDataRepository wordDataRepository;
 
-	@Transactional(readOnly = true)
-	public String findDefinitionByWord(String word) {
-		return wordDataRepository.findByWord(word).orElseThrow().getDefinition();
-	}
+    @Transactional(readOnly = true)
+    public String findDefinitionByWord(String word) {
+        return wordDataRepository.findByWord(word).orElseThrow().getDefinition();
+    }
 
-	@Transactional(readOnly = true)
-	public WordData loadWoadDataById(Long id) {
-		return wordDataRepository.findById(id).orElseThrow(NotExistWordDataException::new);
-	}
+    @Transactional(readOnly = true)
+    public WordData loadWoadDataById(Long id) {
+        return wordDataRepository.findById(id).orElseThrow(NotExistWordDataException::new);
+    }
 
-	@Transactional(readOnly = true)
-	public WordDataDetail.Response loadWordDataDetail(Long id) {
-		WordData wordData = wordDataRepository.findById(id).orElseThrow(NotExistWordDataException::new);
-		List<WordList> wordList = wordListService.loadWordListByWordData(wordData);
-		Collections.shuffle(wordList);
-		List<WordList> topTenList = new ArrayList<>();
-		for (int i = 0; i < 10; i++) {
-			topTenList.add(wordList.get(i));
-		}
-		List<SpeechBasic.Response> speechBasicList = topTenList.stream().map(speechService::loadByWordList).toList();
-		return WordDataMapper.toDetailResponse(wordData, speechBasicList);
-	}
+    @Transactional(readOnly = true)
+    public WordDataDetail.Response loadWordDataDetail(Long id) {
+        WordData wordData = wordDataRepository.findById(id).orElseThrow(NotExistWordDataException::new);
+        List<WordList> wordList = wordListService.loadWordListByWordData(wordData);
+        Collections.shuffle(wordList);
+        List<WordList> topTenList = new ArrayList<>();
+//		620846~621675
+        for (int i = 0; i < wordList.size(); i++) {
+            Long wordListId = wordList.get(i).getId();
+            if (wordListId >= 620846 && wordListId <= 621675) {
+                topTenList.add(wordList.get(i));
+            }
+            if (topTenList.size() >= 10) {
+                break;
+            }
+        }
+        int len = Math.min(wordList.size(), 10 - topTenList.size());
+        for (int i = 0; i < len; i++) {
+            if (topTenList.contains(wordList.get(i))) {
+                continue;
+            }
+            topTenList.add(wordList.get(i));
+        }
+        List<SpeechBasic.Response> speechBasicList = topTenList.stream().map(speechService::loadByWordList).toList();
+        return WordDataMapper.toDetailResponse(wordData, speechBasicList);
+    }
 
-	@Transactional(readOnly = true)
-	public List<WordDataDetail.Response> loadWordDataDetailWithoutSpeech(Long speechId) {
-		List<WordList> wordList = wordListService.loadWordListBySpeechId(speechId);
-		List<WordData> wordDataList = wordList.stream().map(w -> wordDataRepository
-				.findById(w.getWordData().getId()).orElseThrow(NotExistWordDataException::new))
-			.toList();
-		log.info("speechId: {}, wordList : {}, wordData : {}", speechId, wordList, wordDataList);
-		return wordDataList.stream().map(WordDataMapper::toDetailResponse).toList();
-	}
+    @Transactional(readOnly = true)
+    public List<WordDataDetail.Response> loadWordDataDetailWithoutSpeech(Long speechId) {
+        List<WordList> wordList = wordListService.loadWordListBySpeechId(speechId);
+        List<WordData> wordDataList = wordList.stream().map(w -> wordDataRepository
+                        .findById(w.getWordData().getId()).orElseThrow(NotExistWordDataException::new))
+                .toList();
+        log.info("speechId: {}, wordList : {}, wordData : {}", speechId, wordList, wordDataList);
+        return wordDataList.stream().map(WordDataMapper::toDetailResponse).toList();
+    }
 
-	public List<WordCloudData.Response> loadWordCloudData(int page, int size) {
-		Pageable pageable = PageRequest.of(page, size);
-		return wordDataRepository.findAllByOrderByFrequencyDesc(pageable)
-			.stream()
-			.map(WordDataMapper::toWordCloudResponse)
-			.toList();
-	}
+    public List<WordCloudData.Response> loadWordCloudData(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return wordDataRepository.findAllByOrderByFrequencyDesc(pageable)
+                .stream()
+                .map(WordDataMapper::toWordCloudResponse)
+                .toList();
+    }
 
-	public List<WordDataDetail.Response> loadRandomWord() {
-		return wordDataRepository.findRandom().stream().map(WordDataMapper::toDetailResponse).toList();
-	}
+    public List<WordDataDetail.Response> loadRandomWord() {
+        return wordDataRepository.findRandom().stream().map(WordDataMapper::toDetailResponse).toList();
+    }
 
-	public Integer loadLevelByWord(String word) {
-		return wordDataRepository.findLevelByWord(word).orElse(null);
-	}
+    public Integer loadLevelByWord(String word) {
+        return wordDataRepository.findLevelByWord(word).orElse(null);
+    }
 }
